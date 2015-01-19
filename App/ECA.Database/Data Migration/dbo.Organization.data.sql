12e9701febdb9606c99f6726cd8abe26020d2a4d
Tom Morgan <thomas.morgan@buchanan-edwards.com>
2015-01-19 12:27:19 -0500

    Added data load scripts for OrganizationType, Organization and Program

---
+++ App/ECA.Database/Data Migration/dbo.Organization.data.sql
@@ -0,0 +1,3 @@
+SET IDENTITY_INSERT [dbo].[Organization] ON
+INSERT INTO [dbo].[Organization] ([OrganizationId], [OrganizationTypeId], [Description], [Status], [Name], [Website], [History_CreatedBy], [History_CreatedOn], [History_RevisedBy], [History_RevisedOn]) VALUES (1, 1, N'Educational and Cultural Affairs', N'Active', N'ECA', N'http://eca.state.gov', 0, N'1/19/2015 12:00:00 AM -05:00', 0, N'1/19/2015 12:00:00 AM -05:00')
+SET IDENTITY_INSERT [dbo].[Organization] OFF
