7521679ceb405b5eacc6f6fcb1ce4699d72939c5
Tom Morgan <thomas.morgan@buchanan-edwards.com>
2015-02-04 11:00:21 -0500

    Added deployment to websites vice cloud services

---
+++ App/ECA.Data/Migrations/201502041444123_DBSync.cs
@@ -0,0 +1,16 @@
+namespace ECA.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DBSync : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
