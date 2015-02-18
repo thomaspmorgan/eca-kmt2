fb1e150ccdbfba659e6a16687d9207331ad60bb2
Tom Morgan <thomas.morgan@buchanan-edwards.com>
2015-02-18 16:41:37 -0500

    Added MoneyFlowDTO, automapper config, and MoneyFlowController with GetMoneyFlowByProject

--- App/ECA.WebApi/Models/AutoMapperConfig.cs
+++ App/ECA.WebApi/Models/AutoMapperConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using ECA.Data;
 using AutoMapper;
+using System.Text;
 
 namespace ECA.WebApi.Models
 {
@@ -34,6 +35,13 @@ namespace ECA.WebApi.Models
             Mapper.CreateMap<History, HistoryDTO>().ReverseMap();
             Mapper.CreateMap<Contact, ContactDTO>().ReverseMap();
             Mapper.CreateMap<Theme, ThemeDTO>().ReverseMap();
+            Mapper.CreateMap<MoneyFlow, MoneyFlowDTO>()
+                .ForMember(dest => dest.MoneyFlowStatus, opt => opt.MapFrom(p => p.MoneyFlowStatus.MoneyFlowStatusName))
+                .ForMember(dest => dest.MoneyFlowType, opt => opt.MapFrom(p => p.MoneyFlowType.MoneyFlowTypeName))
+                .ForMember(dest => dest.SourceType, opt => opt.MapFrom(p => p.SourceType.TypeName))
+                .ForMember(dest => dest.RecipientType, opt => opt.MapFrom(p => p.RecipientType.TypeName))
+                .ForMember(dest => dest.RecipientName, opt => opt.ResolveUsing<RecipientNameResolver>())
+                .ForMember(dest => dest.SourceName, opt => opt.ResolveUsing<SourceNameResolver>());
             Mapper.AssertConfigurationIsValid();
         }
 
@@ -49,5 +57,80 @@ namespace ECA.WebApi.Models
             }
             return expression;
         }
+
+        public class RecipientNameResolver : ValueResolver<MoneyFlow, string>
+        {
+            protected override string ResolveCore(MoneyFlow source)
+            {
+                string name = "";
+
+                if (source.RecipientProgramId != null)
+                    name = source.RecipientProgram.Name;
+                else if (source.RecipientProjectId != null)
+                    name = source.RecipientProject.Name;
+                else if (source.RecipientTransportationId != null)
+                    name = source.RecipientTransportation.Carrier.Name;
+                else if (source.RecipientItineraryStopId != null)
+                    name = source.RecipientItineraryStopId.ToString(); // Need a name for the ItineraryStop
+                else if (source.RecipientParticipantId != null)
+                {
+                    if (source.RecipientParticipant.OrganizationId != null)
+                        name = source.RecipientParticipant.Organization.Name;
+                    else if (source.RecipientParticipant.PersonId != null)
+                        name = FullName(source.RecipientParticipant.Person.Names);
+                }
+                else if (source.RecipientTransportationId != null)
+                    name = source.RecipientTransportation.Carrier.Name;
+                else if (source.RecipientAccommodationId != null)
+                    name = source.RecipientAccommodation.Host.Name;
+
+                return name;
+            }
+        }
+
+        public class SourceNameResolver : ValueResolver<MoneyFlow, string>
+        {
+            protected override string ResolveCore(MoneyFlow source)
+            {
+                string name = "";
+
+                if (source.SourceProgramId != null)
+                    name = source.RecipientProgram.Name;
+                else if (source.SourceProjectId != null)
+                    name = source.RecipientProject.Name;
+                else if (source.SourceItineraryStopId != null)
+                    name = source.SourceItineraryStopId.ToString(); // Need a name for the ItineraryStop
+                else if (source.SourceParticipantId != null)
+                {
+                    if (source.SourceParticipant.OrganizationId != null)
+                        name = source.SourceParticipant.Organization.Name;
+                    else if (source.SourceParticipant.PersonId != null)
+                        name = FullName(source.SourceParticipant.Person.Names);
+                }
+                return name;
+            }
+        }
+
+        private static string FullName(ICollection<NamePart> Names)
+        {
+            StringBuilder fullName = new StringBuilder();
+            NamePart namePart = Names.FirstOrDefault(p => p.NameType == NameType.GivenName);
+            if (namePart != null)
+                fullName.Append(namePart.Value);
+            namePart = Names.FirstOrDefault(p => p.NameType == NameType.Alias);
+            if (namePart != null)
+                fullName.Append(" '" + namePart.Value + "'");
+            namePart = Names.FirstOrDefault(p => p.NameType == NameType.MiddleName);
+            if (namePart != null)
+                fullName.Append(" " + namePart.Value);
+            namePart = Names.FirstOrDefault(p => p.NameType == NameType.FamilyName);
+            if (namePart != null)
+                fullName.Append(" " + namePart.Value);
+            namePart = Names.FirstOrDefault(p => p.NameType == NameType.Patronym);
+            if (namePart != null)
+                fullName.Append(" " + namePart.Value);
+            return fullName.ToString();
+        }
+
     }
 }
