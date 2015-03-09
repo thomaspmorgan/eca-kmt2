072614ec66f12e54744afa153053e60f586b9100
Brian Gibowski <brian.gibowski@buchanan-edwards.com>
2015-03-09 06:39:42 -0600

    quick code refactor
    Related Work Items: #4666

---
+++ App/ECA.Business/Service/Programs/ProgramServiceValidationEntity.cs
@@ -0,0 +1,71 @@
+﻿using ECA.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECA.Business.Service.Programs
+{
+    /// <summary>
+    /// The ProgramServiceValidationEntity is a container for all objects that must be validated for a Program.
+    /// </summary>
+    public class ProgramServiceValidationEntity
+    {
+        /// <summary>
+        /// Creates a new ProgramServiceValidationEntity.
+        /// </summary>
+        /// <param name="name">The name of the program.</param>
+        /// <param name="description">The description of the program.</param>
+        /// <param name="regionLocationTypeIds">The region ids.</param>
+        /// <param name="focus">The focus.</param>
+        /// <param name="owner">The owner.</param>
+        /// <param name="parentProgramId">The parent program id.</param>
+        /// <param name="parentProgram">The parent program.</param>
+        public ProgramServiceValidationEntity(string name, string description, List<int> regionLocationTypeIds, Focus focus, Organization owner, int? parentProgramId, Program parentProgram)
+        {
+            this.RegionLocationTypeIds = regionLocationTypeIds;
+            this.Focus = focus;
+            this.OwnerOrganization = owner;
+            this.ParentProgramId = parentProgramId;
+            this.ParentProgram = parentProgram;
+            this.Name = name;
+            this.Description = description;
+        }
+
+        /// <summary>
+        /// Gets the region ids.
+        /// </summary>
+        public List<int> RegionLocationTypeIds { get; private set; }
+
+        /// <summary>
+        /// Gets the focus.
+        /// </summary>
+        public Focus Focus { get; private set; }
+
+        /// <summary>
+        /// Gets or the owner.
+        /// </summary>
+        public Organization OwnerOrganization { get; private set; }
+
+        /// <summary>
+        /// Gets the parent program.
+        /// </summary>
+        public Program ParentProgram { get; private set; }
+
+        /// <summary>
+        /// Gets the parent program id.
+        /// </summary>
+        public int? ParentProgramId { get; private set; }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gest the description.
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
--- App/ECA.Business/Service/Programs/ProgramServiceValidator.cs
+++ App/ECA.Business/Service/Programs/ProgramServiceValidator.cs
@@ -12,68 +12,6 @@ using System.Threading.Tasks;
 namespace ECA.Business.Service.Programs
 {
     /// <summary>
-    /// The ProgramServiceValidationEntity is a container for all objects that must be validated for a Program.
-    /// </summary>
-    public class ProgramServiceValidationEntity
-    {
-        /// <summary>
-        /// Creates a new ProgramServiceValidationEntity.
-        /// </summary>
-        /// <param name="name">The name of the program.</param>
-        /// <param name="description">The description of the program.</param>
-        /// <param name="regionLocationTypeIds">The region ids.</param>
-        /// <param name="focus">The focus.</param>
-        /// <param name="owner">The owner.</param>
-        /// <param name="parentProgramId">The parent program id.</param>
-        /// <param name="parentProgram">The parent program.</param>
-        public ProgramServiceValidationEntity(string name, string description, List<int> regionLocationTypeIds, Focus focus, Organization owner, int? parentProgramId, Program parentProgram)
-        {
-            this.RegionLocationTypeIds = regionLocationTypeIds;
-            this.Focus = focus;
-            this.OwnerOrganization = owner;
-            this.ParentProgramId = parentProgramId;
-            this.ParentProgram = parentProgram;
-            this.Name = name;
-            this.Description = description;
-        }
-
-        /// <summary>
-        /// Gets the region ids.
-        /// </summary>
-        public List<int> RegionLocationTypeIds { get; private set; }
-
-        /// <summary>
-        /// Gets the focus.
-        /// </summary>
-        public Focus Focus { get; private set; }
-
-        /// <summary>
-        /// Gets or the owner.
-        /// </summary>
-        public Organization OwnerOrganization { get; private set; }
-
-        /// <summary>
-        /// Gets the parent program.
-        /// </summary>
-        public Program ParentProgram { get; private set; }
-
-        /// <summary>
-        /// Gets the parent program id.
-        /// </summary>
-        public int? ParentProgramId { get; private set; }
-
-        /// <summary>
-        /// Gets the name.
-        /// </summary>
-        public string Name { get; set; }
-
-        /// <summary>
-        /// Gest the description.
-        /// </summary>
-        public string Description { get; set; }
-    }
-
-    /// <summary>
     /// The ProgramServiceValidator is used to validate a Program Business entity on create and update.
     /// </summary>
     public class ProgramServiceValidator : BusinessValidatorBase<ProgramServiceValidationEntity, ProgramServiceValidationEntity>
