9ea48d3b8d2b2433444226c0c089bc5dc9ddb566
Brandon Tucker <brandon.tucker@buchanan-edwards.com>
2015-03-10 11:47:11 -0400

    Created gulp build

---
+++ App/ECA.WebApp.AngularJS/gulpfile.js
@@ -0,0 +1,43 @@
+﻿var gulp = require('gulp');
+
+gulp.task('clean', function (cb) {
+    var del = require('del');
+    del('dist/**/*', cb);
+});
+
+gulp.task('styles', ['clean'], function () {
+    var sass = require('gulp-sass');
+    gulp.src('styles/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/styles'));
+});
+
+
+gulp.task('html', ['clean'], function () {
+    var useref = require('gulp-useref');
+    var gulpIf = require('gulp-if');
+    var uglify = require('gulp-uglify');
+    var minifyCss = require('gulp-minify-css');
+
+    var assets = useref.assets();
+
+    gulp.src('index.html')
+        .pipe(assets)
+        .pipe(gulpIf('*.js', uglify({mangle: false})))
+        .pipe(gulpIf('*.css', minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy', ['clean'], function () {
+    gulp.src('images/**/*.*')
+        .pipe(gulp.dest('dist/images'));
+    gulp.src('views/**/*.*')
+        .pipe(gulp.dest('dist/views'));
+});
+
+gulp.task('default', ['clean', 'styles', 'html', 'copy'], function () {
+
+});
+
