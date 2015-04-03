b22b05eacf8f933c26cf85db7766699ed6f27d7e
Tom Morgan <thomas.morgan@buchanan-edwards.com>
2015-04-03 16:17:02 -0400

    Added Focus/Category and Justification/Objective tables and references from Project and Program
    Related Work Items: #5295

---
+++ App/ECA.Database/dbo/Tables/ProjectCategory.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[ProjectCategory]
+(
+	[ProjectId] INT NOT NULL , 
+    [CategoryId] INT NOT NULL, 
+	CONSTRAINT [PK_dbo.ProjectCategory] PRIMARY KEY CLUSTERED ([ProjectId] ASC, [CategoryId] ASC),
+    CONSTRAINT [FK_dbo.ProjectCategory_dbo.ProjectId] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Project] ([ProjectId]),
+    CONSTRAINT [FK_dbo.ProjectCategory_dbo.CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([CategoryId])
+	);
+
+	
+GO
+CREATE NONCLUSTERED INDEX [IX_ProjectId]
+    ON [dbo].[ProjectCategory]([ProjectId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_CategoryId]
+    ON [dbo].[ProjectCategory]([CategoryId] ASC);
