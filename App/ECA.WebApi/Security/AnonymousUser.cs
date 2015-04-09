9e403079a07ab51f5b58b908cd9f2ba57b6c512a
brian.gibowski <brian.gibowski@buchanan-edwards.com>
2015-04-09 16:15:14 -0500

    code refactors, and started permission caching
    Related Work Items: #5099

---
+++ App/ECA.WebApi/Security/AnonymousUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECA.WebApi.Security
+{
+    public class AnonymousUser : WebApiUserBase
+    {
+        public const string ANONYMOUS_USER_NAME = "Anonymous";
+
+        public AnonymousUser()
+        {
+
+        }
+
+        public Guid Id
+        {
+            get { return Guid.Empty; }
+        }
+
+        public override Business.Service.User ToBusinessUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool HasPermission(ResourcePermission requestedPermission, IEnumerable<ResourcePermission> allUserPermissions)
+        {
+            return false;
+        }
+
+        public override string GetUsername()
+        {
+            return ANONYMOUS_USER_NAME;
+        }
+    }
+}
