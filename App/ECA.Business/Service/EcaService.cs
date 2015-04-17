e0a2da74b88dc15604c9e815abbf72e60e290575
Doug Krehbel <doug.krehbel@buchanan-edwards.com>
2015-04-17 13:59:36 -0400

    Verifying create program

--- App/ECA.Business/Service/EcaService.cs
+++ App/ECA.Business/Service/EcaService.cs
@@ -287,6 +287,24 @@ namespace ECA.Business.Service
         }
 
         /// <summary>
+        /// Updates the regions on the given program to the regions with the given ids.
+        /// </summary>
+        /// <param name="regionIds">The regions by id.</param>
+        /// <param name="programEntity">The program to update.</param>
+        public void SetRegions(List<int> regionIds, Program programEntity)
+        {
+            Contract.Requires(regionIds != null, "The theme ids must not be null.");
+            Contract.Requires(programEntity != null, "The program entity must not be null.");
+            programEntity.Regions.Clear();
+            regionIds.ForEach(x =>
+            {
+                var location = new Location { LocationId = x };
+                this.Context.Locations.Attach(location);
+                programEntity.Regions.Add(location);
+            });
+        }
+
+        /// <summary>
         /// Updates the themes on the given program to the themes with the given ids.  Ensure the themes
         /// are already loaded via the context before calling this method.
         /// </summary>
