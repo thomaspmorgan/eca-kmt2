ce2b513ff5bf1cac291bdd23e1b8e5a5a0e617e2
brian.gibowski <brian.gibowski@buchanan-edwards.com>
2015-04-17 15:32:48 -0500

    build fix test
    Related Work Items: #5099

--- App/ECA.WebApp.AngularJS/build.cmd
+++ App/ECA.WebApp.AngularJS/build.cmd
@@ -1,4 +1,7 @@
 MKDIR C:\Users\buildguest\AppData\Roaming\npm
+RMDIR .\dist /S /Q
+RMDIR .\node_modules /S /Q
+RMDIR .\bowser_components /S /Q
 call .bin\npm install
 call .bin\bower install
 call .bin\node node_modules\gulp\bin\gulp.js
