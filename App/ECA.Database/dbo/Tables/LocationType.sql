102de94ef0dcf1c51f33c926453fcd54b1908b89
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-04-17 13:48:04 -0400

    New scripts and packages and Column length assignments

--- App/ECA.Database/dbo/Tables/LocationType.sql
+++ App/ECA.Database/dbo/Tables/LocationType.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[LocationType] (
     [LocationTypeId]    INT                IDENTITY (1, 1) NOT NULL,
-    [LocationTypeName]  NVARCHAR (MAX)     NOT NULL,
+    [LocationTypeName]  NVARCHAR (50)     NOT NULL,
     [History_CreatedBy] INT                NOT NULL,
     [History_CreatedOn] DATETIMEOFFSET (7) NOT NULL,
     [History_RevisedBy] INT                NOT NULL,
