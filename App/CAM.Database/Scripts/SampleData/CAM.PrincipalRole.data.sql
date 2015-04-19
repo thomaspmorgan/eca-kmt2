abd7bb80d7c3d30f25510a3395672435c11befd1
Tom Morgan <thomas.morgan@buchanan-edwards.com>
2015-04-19 13:01:23 -0400

    Added ECATest2 user to CAM, and to the Project Manager role

--- App/CAM.Database/Scripts/SampleData/CAM.PrincipalRole.data.sql
+++ App/CAM.Database/Scripts/SampleData/CAM.PrincipalRole.data.sql
@@ -1,1 +1,2 @@
 INSERT INTO [CAM].[PrincipalRole] ([PrincipalId], [RoleId], [AssignedBy], [AssignedOn]) VALUES (2, 4, 1, N'4/14/2014 12:00:00 AM -04:00')
+INSERT INTO [CAM].[PrincipalRole] ([PrincipalId], [RoleId], [AssignedBy], [AssignedOn]) VALUES (3, 5, 1, N'4/16/2014 12:00:00 AM -04:00')
