af93d69b96c4ad1c3cfe3474c6792e9b2dde4745
brian.gibowski <brian.gibowski@buchanan-edwards.com>
2015-04-22 08:17:09 -0500

    added missing table in database project
    Related Work Items: #4929

---
+++ App/ECA.Database/dbo/Tables/Log.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Log]
+(
+	[Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Date]       DATETIME       NULL,
+    [Level]      VARCHAR (15)  NULL,
+    [Message]    VARCHAR (MAX) NULL,
+    [Username]   VARCHAR (255) NULL,
+	[Controller]   VARCHAR (255) NULL,
+	[Action]   VARCHAR (255) NULL,
+    [Exception]  VARCHAR (MAX) NULL,
+    [Stacktrace] VARCHAR (MAX) NULL,
+	[Callsite] VARCHAR (MAX) NULL,
+	[ActionArguments] VARCHAR (MAX) NULL, 
+    [RequestId] UNIQUEIDENTIFIER NULL,
+)
