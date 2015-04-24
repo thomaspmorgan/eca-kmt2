3e54080e35d0f45d01e38bdbe29710c3e0285a50
morgant <thomas.morgan@buchanan-edwards.com>
2015-04-24 16:02:03 -0400

    Added report service, first report query and report definition

---
+++ App/ECA.Business/Queries/Reports/ReportQueries.cs
@@ -0,0 +1,29 @@
+﻿using ECA.Business.Queries.Models.Reports;
+using ECA.Core.DynamicLinq;
+using ECA.Data;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace ECA.Business.Queries.Programs
+{
+    public static class ReportQueries
+    {
+        /// <summary>
+        /// Creates a query to return filtered and sorted simple program dtos.
+        /// </summary>
+        /// <param name="context">The context to query.</param>
+        /// <returns>The query to return filtered and sorted simple program dtos.</returns>
+        public static IQueryable<ProjectAwardDTO> CreateGetProjectAward(EcaContext context, int year, int countryId)
+        {
+            Contract.Requires(context != null, "The context must not be null.");
+            var query = context.Projects.Where(p => p.StartDate.Year == year  && p.Locations.FirstOrDefault().CountryId == countryId).Select(x => new ProjectAwardDTO
+            {
+                Title = x.Name,
+                Summary = x.Description,
+                Year = x.StartDate.Year,
+                Award = x.RecipientProjectMoneyFlows.Sum(p => p.Value)
+            });
+            return query;
+        }
+    }
+}
---
+++ App/ECA.Business/Service/Reports/ReportService.cs
@@ -0,0 +1,44 @@
+﻿using ECA.Business.Queries.Models.Programs;
+using ECA.Business.Queries.Programs;
+using ECA.Core.DynamicLinq;
+using ECA.Core.Query;
+using ECA.Core.Service;
+using ECA.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+using ECA.Business.Queries.Models.Reports;
+using NLog;
+
+namespace ECA.Business.Service.Reports
+{
+    public class ReportService : DbContextService<EcaContext>, IReportService
+    {
+        private readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public ReportService(EcaContext context)
+            : base(context)
+        {
+            Contract.Requires(context != null, "The context must not be null");
+        }
+
+        #region Get
+        public IQueryable<ProjectAwardDTO> GetProjectAwards(int year, int countryId)
+        {
+            logger.Trace("Getting Project Awards for Year: [{0}], CountryId [{0}]", year, countryId);
+            return this.GetProjectAwardDTOQuery(year, countryId);
+        }
+
+        #endregion
+
+        #region Queries
+        protected IQueryable<ProjectAwardDTO> GetProjectAwardDTOQuery(int year, int countryId)
+        {
+            return ReportQueries.CreateGetProjectAward(this.Context, year, countryId);
+        }
+        #endregion
+    }
+}
