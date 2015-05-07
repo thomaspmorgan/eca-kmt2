fa561f85bcd0b8fb3992e4c8eb7d38e229714c35
Doug Krehbel <doug.krehbel@buchanan-edwards.com>
2015-05-07 13:00:02 -0400

    Fix to GetPrograms and adding new function for ChildPrograms

---
+++ App/ECA.Database/dbo/Functions/NumberOfChildPrograms.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION [dbo].[NumberOfChildPrograms] 
+(
+	@programId int
+)
+RETURNS int
+AS
+BEGIN
+
+	DECLARE @numChildren int
+
+	set @numChildren = (Select count(*) from program where 
+		parentProgram_ProgramId = @programId and programstatusID = 1);
+
+	Return @numChildren
+
+	END
+
+	GO
+
