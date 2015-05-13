d9c12dc799070c545787ed541e6adfa4e5c04fda
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-05-13 11:06:18 -0400

    Itinerary table changes and modified packages

---
+++ App/ECA.Database/dbo/Tables/ItineraryStopPerson.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[ItineraryStopPerson] (
+    [ItineraryStopId] INT NOT NULL,
+    [PersonId]   INT NOT NULL,
+    CONSTRAINT [PK_dbo.ItineraryStopPerson] PRIMARY KEY CLUSTERED ([ItineraryStopId] ASC, [PersonId] ASC),
+    CONSTRAINT [FK_dbo.ItineraryStopPerson_dbo.ItineraryStop_ItineraryStopId] FOREIGN KEY ([ItineraryStopId]) REFERENCES [dbo].[ItineraryStop] ([ItineraryStopId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.ItineraryStopPerson_dbo.Person_PersonId] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([PersonId]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ItineraryStopId]
+    ON [dbo].[ItineraryStopPerson]([ItineraryStopId] ASC);
+
+
+GO
+
+
+CREATE NONCLUSTERED INDEX [IX_PersonId] ON [dbo].[ItineraryStopPerson] ([PersonId] ASC)
