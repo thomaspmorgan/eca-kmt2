6be7108be1d7ce9de5e36acb5a300392b40220d3
Doug Krehbel <doug.krehbel@buchanan-edwards.com>
2015-05-15 13:02:36 -0400

    Changes to SPs for program list

---
+++ App/ECA.Database/dbo/Stored Procedures/GetProgramsForUserAlpha.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE GetProgramsForUserAlpha
+(@UserId int)
+AS
+
+select * from program 
+where programstatusid = 1
+OR programStatusID = 4 and History_CreatedBy = @userID
+order by name
