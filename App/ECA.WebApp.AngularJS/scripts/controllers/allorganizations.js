de87b1102e5212ed31615828fbe68c6408a0f110
Brandon Tucker <brandon.tucker@buchanan-edwards.com>
2015-05-20 14:34:27 -0400

    Scaffolded all organizations page and controller
    Related Work Items: #6841

---
+++ App/ECA.WebApp.AngularJS/scripts/controllers/allorganizations.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+/**
+ * @ngdoc function
+* @name staticApp.controller:AllOrganizationsCtrl
+ * @description
+ * # AllOrganizationsCtrl
+ * Controller of the staticApp
+ */
+angular.module('staticApp')
+  .controller('AllOrganizationsCtrl', function ($scope, $stateParams) {
+
+      $scope.organizations = [];
+      $scope.start = 0;
+      $scope.end = 0;
+      $scope.total = 0;
+  });
