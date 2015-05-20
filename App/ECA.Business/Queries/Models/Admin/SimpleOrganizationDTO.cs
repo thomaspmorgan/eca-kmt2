20c1b1f12791ddb93e4b0080fa59289d564bc7fd
Brandon Tucker <brandon.tucker@buchanan-edwards.com>
2015-05-20 17:45:17 -0400

    Created web api and service for get organizations

---
+++ App/ECA.Business/Queries/Models/Admin/SimpleOrganizationDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECA.Business.Queries.Models.Admin
+{
+    /// <summary>
+    /// Simple dto for organization
+    /// </summary>
+    public class SimpleOrganizationDTO
+    {
+        /// <summary>
+        /// Gets or sets the organization id
+        /// </summary>
+        public int OrganizationId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the organization type
+        /// </summary>
+        public string OrganizationType { get; set; }
+
+        /// <summary>
+        /// Gets or set the status
+        /// </summary>
+        public string Status { get; set; }
+    }
+}
---
+++ App/ECA.Business/Service/Admin/IOrganizationService.cs
@@ -0,0 +1,24 @@
+﻿using ECA.Business.Queries.Models.Admin;
+using ECA.Core.DynamicLinq;
+using ECA.Core.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECA.Business.Service.Admin
+{
+    /// <summary>
+    /// Interface for organization service
+    /// </summary>
+    public interface IOrganizationService
+    {
+        /// <summary>
+        /// Returns a list of organizations asyncronously
+        /// </summary>
+        /// <param name="queryOperator">The query operator to apply</param>
+        /// <returns>List of organizations</returns>
+        Task<PagedQueryResults<SimpleOrganizationDTO>> GetOrganizationsAsync(QueryableOperator<SimpleOrganizationDTO> queryOperator);
+    }
+}
