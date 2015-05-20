9a8bef78e482e23dca7913f1a31d69b7106a32cb
Brandon Tucker <brandon.tucker@buchanan-edwards.com>
2015-05-20 18:21:45 -0400

    Ui for organization list

--- App/ECA.WebApp.AngularJS/scripts/controllers/allorganizations.js
+++ App/ECA.WebApp.AngularJS/scripts/controllers/allorganizations.js
@@ -8,10 +8,38 @@
  * Controller of the staticApp
  */
 angular.module('staticApp')
-  .controller('AllOrganizationsCtrl', function ($scope, $stateParams) {
+  .controller('AllOrganizationsCtrl', function ($scope, $stateParams, OrganizationService, TableService) {
 
       $scope.organizations = [];
       $scope.start = 0;
       $scope.end = 0;
       $scope.total = 0;
+
+      $scope.organizationsLoading = false;
+
+      $scope.getOrganizations = function (tableState) {
+
+          $scope.organizationsLoading = true;
+
+          TableService.setTableState(tableState);
+
+          var params = {
+              start: TableService.getStart(),
+              limit: TableService.getLimit(),
+              sort: TableService.getSort(),
+              filter: TableService.getFilter()
+          };
+
+          OrganizationService.getOrganizations(params)
+            .then(function (data) {
+                $scope.organizations = data.results;
+                var limit = TableService.getLimit();
+                var start = TableService.getStart();
+                tableState.pagination.numberOfPages = Math.ceil(data.total / limit);
+                $scope.start = start + 1;
+                $scope.end = start + data.results.length;
+                $scope.total = data.total;
+                $scope.organizationsLoading = false;
+            });
+      };
   });
