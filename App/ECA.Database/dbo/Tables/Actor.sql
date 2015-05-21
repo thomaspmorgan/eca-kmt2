b42813e902fbe9c6a70bc5a6e7184dea3f35d0d7
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-05-21 10:22:34 -0400

    Added to existing SSIS packages and some DB changes

--- App/ECA.Database/dbo/Tables/Actor.sql
+++ App/ECA.Database/dbo/Tables/Actor.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Actor] (
     [ActorId]           INT                IDENTITY (1, 1) NOT NULL,
     [ActorTypeId]       INT                NOT NULL,
-    [ActorName]         NVARCHAR (MAX)     NOT NULL,
-    [Status]            NVARCHAR (MAX)     NULL,
-    [Action]            NVARCHAR (MAX)     NULL,
+    [ActorName]         NVARCHAR (600)     NOT NULL,
+    [Status]            NVARCHAR (100)     NULL,
+    [Action]            NVARCHAR (100)     NULL,
     [PersonId]          INT                NULL,
     [OrganizationId]    INT                NULL,
     [EventId]           INT                NULL,
@@ -13,13 +13,15 @@
     [History_RevisedBy] INT                NOT NULL,
     [History_RevisedOn] DATETIMEOFFSET (7) NOT NULL,
     [AppointmentId] INT NULL, 
+    [ProjectId] INT NULL, 
     CONSTRAINT [PK_dbo.Actor] PRIMARY KEY CLUSTERED ([ActorId] ASC),
     CONSTRAINT [FK_dbo.Actor_dbo.ActorType_ActorTypeId] FOREIGN KEY ([ActorTypeId]) REFERENCES [dbo].[ActorType] ([ActorTypeId]) ON DELETE CASCADE,
     CONSTRAINT [FK_dbo.Actor_dbo.Event_EventId] FOREIGN KEY ([EventId]) REFERENCES [dbo].[Event] ([EventId]),
     CONSTRAINT [FK_dbo.Actor_dbo.ItineraryStop_ItineraryStopId] FOREIGN KEY ([ItineraryStopId]) REFERENCES [dbo].[ItineraryStop] ([ItineraryStopId]),
     CONSTRAINT [FK_dbo.Actor_dbo.Organization_OrganizationId] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[Organization] ([OrganizationId]),
     CONSTRAINT [FK_dbo.Actor_dbo.Person_PersonId] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([PersonId]), 
-    CONSTRAINT [FK_dbo.Actor_dbo.ItineraryAppointment_AppointmentId] FOREIGN KEY ([AppointmentId]) REFERENCES [dbo].[ItineraryAppointment]([AppointmentId])
+    CONSTRAINT [FK_dbo.Actor_dbo.ItineraryAppointment_AppointmentId] FOREIGN KEY ([AppointmentId]) REFERENCES [dbo].[ItineraryAppointment]([AppointmentId]), 
+    CONSTRAINT [FK_dbo.Actor_dbo.Project_ProjectId] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Project]([ProjectId])
 );
 
 
@@ -51,3 +53,7 @@ CREATE NONCLUSTERED INDEX [IX_ActorTypeId]
 GO
 
 CREATE INDEX [IX_AppointmentId] ON [dbo].[Actor] ([AppointmentId])
+
+GO
+
+CREATE INDEX [IX_ProjectId] ON [dbo].[Actor] ([ProjectId])
