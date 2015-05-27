35a2088a700685196862289f369bb6ab1926a9e5
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-05-26 22:22:54 -0400

    New SSIS Packages to load SEVIS lookup data

--- App/SEVIS.Database/SEVIS/Schema/SevisSchema.sql
+++ App/SEVIS.Database/SEVIS/Schema/SevisSchema.sql
@@ -1,6 +1,3 @@
-﻿USE [ECA_Dev]
-GO
-
-/****** Object:  Schema [sevis]    Script Date: 5/22/2015 1:07:30 PM ******/
-CREATE SCHEMA [sevis]
+﻿/****** Object:  Schema [sevis]    Script Date: 5/22/2015 1:07:30 PM ******/
+CREATE SCHEMA sevis
 GO
