1db5f6d48a9aa718b76f238de412fc535ef9d5b7
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-05-28 09:26:00 -0400

    Added new DB Objects

---
+++ App/ECA.Database/Sevis/Tables/EducationLevel.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [sevis].[EducationLevel]
+(
+	[EducationLevelId] INT IDENTITY(1,1) NOT NULL, 
+    [EducationLevelCode] NVARCHAR(3) NOT NULL, 
+    [Description] NVARCHAR(100) NOT NULL, 
+    [F_1_Ind] CHAR NOT NULL, 
+    [M_1_Ind] CHAR NOT NULL, 
+    [History_CreatedBy] INT NOT NULL, 
+    [History_CreatedOn] DATETIMEOFFSET NOT NULL, 
+    [History_RevisedBy] INT NOT NULL, 
+    [History_RevisedOn] DATETIMEOFFSET NOT NULL, 
+    CONSTRAINT [PK_sevis.EducationLevel] PRIMARY KEY ([EducationLevelId])
+)
+
+GO
+
+CREATE INDEX [IX_EducationLevelCode] ON [sevis].[EducationLevel] ([EducationLevelCode])
