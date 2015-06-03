f9356c58ce30df1530215bb5e7768b9882e751b9
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-06-03 11:42:05 -0400

    Updated packages, new packages, modified DB tables, new sample data

--- App/ECA.Database/dbo/Tables/LanguageProficiencyPerson.sql
+++ App/ECA.Database/dbo/Tables/LanguageProficiencyPerson.sql
@@ -1,9 +1,13 @@
 ﻿CREATE TABLE [dbo].[LanguageProficiencyPerson] (
     [LanguageProficiency_LanguageProficiencyId] INT NOT NULL,
     [Person_PersonId]                           INT NOT NULL,
+    [NativeLanguageInd] CHAR NOT NULL, 
+    [SpeakingProficiency] INT NULL, 
+    [ReadingProficiency] INT NULL, 
+    [ComprehensionProficiency] INT NULL, 
     CONSTRAINT [PK_dbo.LanguageProficiencyPerson] PRIMARY KEY CLUSTERED ([LanguageProficiency_LanguageProficiencyId] ASC, [Person_PersonId] ASC),
     CONSTRAINT [FK_dbo.LanguageProficiencyPerson_dbo.LanguageProficiency_LanguageProficiency_LanguageProficiencyId] FOREIGN KEY ([LanguageProficiency_LanguageProficiencyId]) REFERENCES [dbo].[LanguageProficiency] ([LanguageProficiencyId]) ON DELETE CASCADE,
-    CONSTRAINT [FK_dbo.LanguageProficiencyPerson_dbo.Person_Person_PersonId] FOREIGN KEY ([Person_PersonId]) REFERENCES [dbo].[Person] ([PersonId]) ON DELETE CASCADE
+    CONSTRAINT [FK_dbo.LanguageProficiencyPerson_dbo.Person_Person_PersonId] FOREIGN KEY ([Person_PersonId]) REFERENCES [dbo].[Person] ([PersonId]) ON DELETE CASCADE 
 );
 
 
