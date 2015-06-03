422ce02477e1e0967518f7c86556b39af3c41a37
brian.gibowski <brian.gibowski@buchanan-edwards.com>
2015-06-03 15:17:34 -0500

    committed missing changes
    Related Work Items: #6818

--- App/ECA.Business/Service/Admin/FocusCategoryService.cs
+++ App/ECA.Business/Service/Admin/FocusCategoryService.cs
@@ -43,7 +43,7 @@ namespace ECA.Business.Service.Admin
         /// <returns>The focus categories.</returns>
         public PagedQueryResults<FocusCategoryDTO> GetFocusCategoriesByOfficeId(int officeId, QueryableOperator<FocusCategoryDTO> queryOperator)
         {
-            var results = FocusCategoryQueries.CreateGetFocusCategoryDTOQuery(this.Context, officeId, queryOperator).ToPagedQueryResults(queryOperator.Start, queryOperator.Limit);
+            var results = FocusCategoryQueries.CreateGetFocusCategoryDTOByOfficeIdQuery(this.Context, officeId, queryOperator).ToPagedQueryResults(queryOperator.Start, queryOperator.Limit);
             logger.Trace("Retrieved focus categories for office with id [{0}].", officeId);
             return results;
         }
@@ -56,8 +56,34 @@ namespace ECA.Business.Service.Admin
         /// <returns>The focus categories.</returns>
         public async Task<PagedQueryResults<FocusCategoryDTO>> GetFocusCategoriesByOfficeIdAsync(int officeId, QueryableOperator<FocusCategoryDTO> queryOperator)
         {
-            var results = await FocusCategoryQueries.CreateGetFocusCategoryDTOQuery(this.Context, officeId, queryOperator).ToPagedQueryResultsAsync(queryOperator.Start, queryOperator.Limit);
+            var results = await FocusCategoryQueries.CreateGetFocusCategoryDTOByOfficeIdQuery(this.Context, officeId, queryOperator).ToPagedQueryResultsAsync(queryOperator.Start, queryOperator.Limit);
             logger.Trace("Retrieved focus categories for office with id [{0}].", officeId);
+            return results;
+        }
+
+        /// <summary>
+        /// Returns the focus categories for the program with the given id.
+        /// </summary>
+        /// <param name="officeId">The program by id.</param>
+        /// <param name="queryOperator">The query operator.</param>
+        /// <returns>The focus categories.</returns>
+        public PagedQueryResults<FocusCategoryDTO> GetFocusCategoriesByProgramId(int programId, QueryableOperator<FocusCategoryDTO> queryOperator)
+        {
+            var results = FocusCategoryQueries.CreateGetFocusCategoryDTOByProgramIdQuery(this.Context, programId, queryOperator).ToPagedQueryResults(queryOperator.Start, queryOperator.Limit);
+            logger.Trace("Retrieved focus categories for program with id [{0}].", programId);
+            return results;
+        }
+
+        /// <summary>
+        /// Returns the focus categories for the program with the given id.
+        /// </summary>
+        /// <param name="officeId">The program by id.</param>
+        /// <param name="queryOperator">The query operator.</param>
+        /// <returns>The focus categories.</returns>
+        public async Task<PagedQueryResults<FocusCategoryDTO>> GetFocusCategoriesByProgramIdAsync(int programId, QueryableOperator<FocusCategoryDTO> queryOperator)
+        {
+            var results = await FocusCategoryQueries.CreateGetFocusCategoryDTOByProgramIdQuery(this.Context, programId, queryOperator).ToPagedQueryResultsAsync(queryOperator.Start, queryOperator.Limit);
+            logger.Trace("Retrieved focus categories for program with id [{0}].", programId);
             return results;
         }
         #endregion
