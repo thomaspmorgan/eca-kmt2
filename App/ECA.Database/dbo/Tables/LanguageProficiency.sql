c22dfb8f1d517ccab1ae4eb41063fb9b3ec4da0d
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-06-03 12:11:03 -0400

    Apply length to language proficiency table

--- App/ECA.Database/dbo/Tables/LanguageProficiency.sql
+++ App/ECA.Database/dbo/Tables/LanguageProficiency.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[LanguageProficiency] (
     [LanguageProficiencyId] INT                IDENTITY (1, 1) NOT NULL,
-    [LanguageName]          NVARCHAR (MAX)     NOT NULL,
+    [LanguageName]          NVARCHAR (100)     NOT NULL,
     [History_CreatedBy]     INT                NOT NULL,
     [History_CreatedOn]     DATETIMEOFFSET (7) NOT NULL,
     [History_RevisedBy]     INT                NOT NULL,
