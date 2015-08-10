b047739b5071b0ee7de5d4769bb8c18fdca94860
thomaspmorgan <thomas.morgan@buchanan-edwards.com>
2015-08-10 10:07:50 -0400

    Updated key to be composite

--- App/ECA.Database/dbo/Tables/PersonProminentCategory.sql
+++ App/ECA.Database/dbo/Tables/PersonProminentCategory.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[PersonProminentCategory]
 (
-	[PersonId] INT NOT NULL PRIMARY KEY, 
+	[PersonId] INT NOT NULL , 
     [ProminentCategoryId] INT NOT NULL, 
     CONSTRAINT [FK_PersonProminentCategory_Person] FOREIGN KEY ([PersonId]) REFERENCES [Person]([PersonId]), 
-    CONSTRAINT [FK_PersonProminentCategory_ProminentCategory] FOREIGN KEY ([ProminentCategoryId]) REFERENCES [ProminentCategory]([ProminentCategoryId])
+    CONSTRAINT [FK_PersonProminentCategory_ProminentCategory] FOREIGN KEY ([ProminentCategoryId]) REFERENCES [ProminentCategory]([ProminentCategoryId]), 
+    PRIMARY KEY ([ProminentCategoryId], [PersonId])
 )
