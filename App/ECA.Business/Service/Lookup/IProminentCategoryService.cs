697d741a9b148f6a644bb74926b64aa4294e7557
thomaspmorgan <thomas.morgan@buchanan-edwards.com>
2015-08-10 08:57:44 -0400

    Add missing files to project

---
+++ App/ECA.Business/Service/Lookup/IProminentCategoryService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ECA.Business.Service.Lookup
+{
+    /// <summary>
+    /// An IParticipantTypeService is capable of performing crud operations on prominent categories.
+    /// </summary>
+    public interface IProminentCategoryService
+    {
+        /// <summary>
+        /// Returns paged, filtered, and sorted prominent categories in the system.
+        /// </summary>
+        /// <param name="queryOperator">The query operator.</param>
+        /// <returns>The prominent categories  in the system.</returns>
+        ECA.Core.Query.PagedQueryResults<ECA.Business.Service.Lookup.ProminentCategoryDTO> Get(ECA.Core.DynamicLinq.QueryableOperator<ECA.Business.Service.Lookup.ProminentCategoryDTO> queryOperator);
+
+        /// <summary>
+        /// Returns paged, filtered, and sorted prominent categories in the system.
+        /// </summary>
+        /// <param name="queryOperator">The query operator.</param>
+        /// <returns>The prominent categories in the system.</returns>
+        System.Threading.Tasks.Task<ECA.Core.Query.PagedQueryResults<ECA.Business.Service.Lookup.ProminentCategoryDTO>> GetAsync(ECA.Core.DynamicLinq.QueryableOperator<ECA.Business.Service.Lookup.ProminentCategoryDTO> queryOperator);
+    }
+}
