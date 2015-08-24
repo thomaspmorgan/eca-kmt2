088ce80ffdff5b6f39baf9537e2e2071254c922c
thomaspmorgan <thomas.morgan@buchanan-edwards.com>
2015-08-23 20:00:37 -0400

    Added missing file.

---
+++ App/ECA.Database/Data Migration/MigrationScripts/ECA/Rename LanguageProficiency to Language.sql
@@ -0,0 +1,10 @@
+﻿/* Change Language Proficiency and Language tables for Sprint 18 */
+
+/* Change the name of the LanguageProficiency table to Language */
+EXEC sp_rename 'dbo.LanguageProficiency', 'Language';                    
+
+GO
+
+EXEC sp_rename 'dbo.LanguageProficiencyPerson', 'PersonLanguageProficiency'; 
+
+GO   
