e328ef72f952a03b694c7a682fa1126093a6369b
Brandon Tucker <brandon.tucker@buchanan-edwards.com>
2015-08-25 11:06:31 -0400

    Reverting file

--- App/ECA.WebApp.AngularJS/build.cmd
+++ App/ECA.WebApp.AngularJS/build.cmd
@@ -1,4 +1,4 @@
 MKDIR C:\Users\buildguest\AppData\Roaming\npm
-call npm install
+call .bin\npm install
 call .bin\bower install
-call node node_modules\gulp\bin\gulp.js
+call .bin\node node_modules\gulp\bin\gulp.js
