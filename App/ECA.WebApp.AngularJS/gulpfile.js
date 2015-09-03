d98fcba8427f02fb254e2e75875cb60c1e3593e0
briangibowski <brian.gibowski@buchanan-edwards.com>
2015-09-03 15:34:40 -0500

    build fixes
    Related Work Items: #8989

--- App/ECA.WebApp.AngularJS/gulpfile.js
+++ App/ECA.WebApp.AngularJS/gulpfile.js
@@ -51,10 +51,8 @@ gulp.task('copy', ['clean'], function () {
         .pipe(gulp.dest('dist/views'));
     gulp.src('bower_components/fonts-raleway/fonts/fonts-raleway/**/*.ttf')
         .pipe(gulp.dest('dist/fonts/fonts-raleway'));
-
     gulp.src('bower_components/material-design-icons/iconfont/*.*')
-        .pipe(gulp.dest('dist/fonts/material-design-icons/iconfont/'));
-
+        .pipe(gulp.dest('dist/bower_components/material-design-icons/iconfont/*.*'));
     gulp.src('bower_components/select2/*.png')
         .pipe(gulp.dest('dist/images'));
 });
