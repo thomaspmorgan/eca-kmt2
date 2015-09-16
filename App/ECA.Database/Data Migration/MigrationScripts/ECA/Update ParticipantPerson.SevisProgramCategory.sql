96585bc6ccf42b2fc60484bb2479d4356f5dc945
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-09-15 23:36:42 -0400

    ParticipantPerson table changes & data migration script additions

---
+++ App/ECA.Database/Data Migration/MigrationScripts/ECA/Update ParticipantPerson.SevisProgramCategory.sql
@@ -0,0 +1,11 @@
+/* Updates programcategoryid for participantperson table */
+UPDATE
+    pp
+SET
+    pp.programcategoryidid = x.programcategoryid
+FROM
+    eca_kmt_dev.eca_kmt_dev.dbo.participantperson pp
+INNER JOIN
+    dbo.dev_xref_Participantperson_update x 
+ON
+    x.eca_participantid = pp.participantid
