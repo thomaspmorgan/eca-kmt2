30fe5c1a7b289332d64eda893b7559c367fda334
Brandon Tucker <brandon.tucker@buchanan-edwards.com>
2015-09-18 13:31:44 -0400

    Added database tables
    Related Work Items: #9169

---
+++ App/ECA.Database/dbo/Tables/OrganizationOrganizationRole.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[OrganizationOrganizationRole]
+(
+	[OrganizationId] INT NOT NULL,
+    [OrganizationRoleId] INT NOT NULL,
+	CONSTRAINT [FK_OrganizationOrganizationRole_Organization] FOREIGN KEY ([OrganizationId]) REFERENCES [Organization]([OrganizationId]), 
+    CONSTRAINT [FK_OrganizationOrganizationRole_OrganizationRole] FOREIGN KEY ([OrganizationRoleId]) REFERENCES [OrganizationRole]([OrganizationRoleId]), 
+    PRIMARY KEY ([OrganizationId], [OrganizationRoleId])
+)
