31dd228f999d6765c89fd0ef9e79724caefdeddc
Wassim Boustani <wassim.boustani@buchanan-edwards.com>
2015-09-22 18:49:22 -0400

    Added fiscal year dropdown directive template to funding pages.

--- App/ECA.WebApp.AngularJS/scripts/directives/yearDropdown.js
+++ App/ECA.WebApp.AngularJS/scripts/directives/yearDropdown.js
@@ -6,24 +6,27 @@
  * @description
  * # lists years in a dropdown according to parameters
  * 
- * Usage: <div year-dropdown model="fiscalYear" offset="-2" range="10"></div>
+ * Usage: <div year-dropdown ng-model="fiscalYear" offset="-10" range="2"></div>
  * 
  */
-angular.module('staticApp', []).directive('yearDropdown', function () {
-    var currentYear = new Date().getFullYear();
-    return {
-        scope: {
-            model: "=ngModel"
-        },
-        require: 'ngModel',
-        replace: true,
-        link: function (scope, element, attrs) {
-            scope.years = [];
-            for (var i = +attrs.offset; i < +attrs.range + 1; i++) {
-                scope.years.push(currentYear + i);
+angular.module('staticApp')
+    .directive('yearDropdown', function () {
+        var currentYear = new Date().getFullYear();
+        return {
+            restrict: 'E',
+            scope: {
+                ngModel: "="
+            },
+            template: function (element, attrs) {
+                return '<select ng-model="' + attrs.ngModel + '" class="form-control">' +
+                '<option ng-selected="{{ngModel == year}}" ng-repeat="year in years" value="{{year}}">{{year}}</option>' +
+                '</select>';
+            },
+            link: function (scope, element, attrs) {
+                scope.years = [];
+                for (var i = +attrs.offset; i < +attrs.range + 1; i++) {
+                    scope.years.push(currentYear + i);
+                }
             }
-            scope.model = currentYear;
-        },
-        template: '<select ng-model="model" ng-options="y for y in years"></select>'
-    }
-});
+        }
+    });
