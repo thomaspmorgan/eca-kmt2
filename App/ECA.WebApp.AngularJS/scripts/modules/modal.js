26f9fb7d2b6b084820dcc91e82b836ac16acee22
Wassim Boustani <wassim.boustani@buchanan-edwards.com>
2015-09-24 14:29:09 -0400

    Fix for IE input x fooling validation. Fixed search dialog close error.

--- App/ECA.WebApp.AngularJS/scripts/modules/modal.js
+++ App/ECA.WebApp.AngularJS/scripts/modules/modal.js
@@ -55,11 +55,11 @@
             }
           };
           scope.hideModal = function() {
-            var close = scope.beforeClose(scope);
-            if (close) {
+            //var close = scope.beforeClose;
+            //if (close) {
               element[0].children[1].scrollTop = 0;
               return scope.show = false;
-            }
+            //}
           };
           scope.$watch('show', function(newVal, oldVal) {
             element[0].children[1].scrollTop = 0;
