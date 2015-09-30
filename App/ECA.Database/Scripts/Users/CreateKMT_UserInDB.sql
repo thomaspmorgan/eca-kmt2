1a90f68a6ef7fbaf6d3868adac3beaff5f2bde30
thomaspmorgan <thomas.morgan@buchanan-edwards.com>
2015-09-30 08:13:28 -0400

    Added create KMT_User in db

---
+++ App/ECA.Database/Scripts/Users/CreateKMT_UserInDB.sql
@@ -0,0 +1,14 @@
+﻿-- =================================================
+-- Create User as DBO template for Windows Azure SQL Database
+-- =================================================
+-- For login <login_name, sysname, login_name>, create a user in the database
+CREATE USER KMT_User
+	FOR LOGIN KMT_User
+	WITH DEFAULT_SCHEMA = dbo
+GO
+
+-- Add user to the database owner role
+EXEC sp_addrolemember N'db_datareader', N'KMT_User'
+EXEC sp_addrolemember N'db_datawriter', N'KMT_User'
+
+GO
