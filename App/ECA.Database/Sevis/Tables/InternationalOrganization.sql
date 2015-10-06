3dc77663f10599dd2d5b871d23e678454376cd11
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-10-06 17:54:27 -0400

    New Sevis Tables; SSIS Load packages for them.

---
+++ App/ECA.Database/Sevis/Tables/InternationalOrganization.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [sevis].[InternationalOrganization]
+(
+	[OrganizationId] INT IDENTITY(1,1) NOT NULL, 
+    [OrganizationCode] NVARCHAR(10) NOT NULL, 
+    [Description] NVARCHAR(250) NOT NULL, 
+    [History_CreatedBy] INT NOT NULL, 
+    [History_CreatedOn] DATETIMEOFFSET NOT NULL, 
+    [History_RevisedBy] INT NOT NULL, 
+    [History_RevisedOn] DATETIMEOFFSET NOT NULL, 
+    CONSTRAINT [PK_sevis.InternationalOrganization] PRIMARY KEY ([OrganizationId]) 
+)
+
+GO
+
+
+
+CREATE INDEX [IX_InternationalOrganization_OrganizationCode] ON [sevis].[InternationalOrganization] ([OrganizationCode])
+
+GO
