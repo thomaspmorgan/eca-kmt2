2cc4837e13160caaa6335db416ebae81e7f58156
briangibowski <brian.gibowski@buchanan-edwards.com>
2015-10-07 15:52:02 -0500

    added unit test project
    Related Work Items: #9517

---
+++ App/ECA.WebJobs.Search.Index.Test/FunctionsTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ECA.WebJobs.Search.Index.Test
+{
+    [TestClass]
+    public class FunctionsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
