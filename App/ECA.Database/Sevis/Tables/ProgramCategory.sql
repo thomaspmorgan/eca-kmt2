be0a01c79b8132396f4957528f7a925660df7d43
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-10-09 19:09:08 -0400

    Restoe Not Null defaulst to sevis.ProgramCategory and sevis.USGovernmentAgency tables

--- App/ECA.Database/Sevis/Tables/ProgramCategory.sql
+++ App/ECA.Database/Sevis/Tables/ProgramCategory.sql
@@ -2,7 +2,7 @@
 (
 	[ProgramCategoryId] INT IDENTITY(1,1) NOT NULL, 
     [ProgramCategoryCode] CHAR(2) NOT NULL, 
-    [Description] NVARCHAR(100) NULL, 
+    [Description] NVARCHAR(100) NOT NULL, 
     [History_CreatedBy] INT NOT NULL, 
     [History_CreatedOn] DATETIMEOFFSET NOT NULL, 
     [History_RevisedBy] INT NOT NULL, 
