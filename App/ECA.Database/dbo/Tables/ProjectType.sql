a1d76f1096f52417d08c6a8eaad4fe9992c62d86
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-10-13 16:29:31 -0400

    Alter length of ProjectType.ProjecttypeName column to support IVLP Project Types.

--- App/ECA.Database/dbo/Tables/ProjectType.sql
+++ App/ECA.Database/dbo/Tables/ProjectType.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[ProjectType] (
     [ProjectTypeId]     INT                IDENTITY (1, 1) NOT NULL,
-    [ProjectTypeName]   NVARCHAR (20)      NOT NULL,
+    [ProjectTypeName]   NVARCHAR (50)      NOT NULL,
     [History_CreatedBy] INT                NOT NULL,
     [History_CreatedOn] DATETIMEOFFSET (7) NOT NULL,
     [History_RevisedBy] INT                NOT NULL,
