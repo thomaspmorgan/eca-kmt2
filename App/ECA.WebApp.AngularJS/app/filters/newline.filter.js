6fd14159575058cf491d63cce6d32f6ceba1b2ef
Wassim Boustani <wassim.boustani@buchanan-edwards.com>
2015-10-15 17:49:38 -0400

    Added filter to remove line breaks from json response.

---
+++ App/ECA.WebApp.AngularJS/app/filters/newline.filter.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+
+angular.module('staticApp')
+    .filter('newline', function ($sce) {
+        return function (text) {
+            return text ? $sce.trustAsHtml(text.toString().replace(/(?:\\[rn]|[\r\n]+)+/g, '<br/>')) : '';
+        }
+    });
