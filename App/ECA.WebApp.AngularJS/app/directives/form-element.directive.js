e601ed82546ffefaee44c873699c6c139991e18c
Wassim Boustani <wassim.boustani@buchanan-edwards.com>
2015-10-15 08:58:16 -0400

    Fix to snapshot budget error.

--- App/ECA.WebApp.AngularJS/app/directives/form-element.directive.js
+++ App/ECA.WebApp.AngularJS/app/directives/form-element.directive.js
@@ -31,22 +31,3 @@ angular.module('staticApp')
            }
        };
    });
-
-/*
- * Sets focus on specific input (usage: focus-me="true")
- */
-angular.module('staticApp')
-  .directive('focusMe', function ($timeout) {
-      return {
-          scope: { trigger: '@focusMe' },
-          link: function (scope, element) {
-              scope.$watch('trigger', function (value) {
-                  if (value === "true") {
-                      $timeout(function () {
-                          element[0].focus();
-                      });
-                  }
-              });
-          }
-      };
-  });
