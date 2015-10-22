b23edeea382c6ca3fbbb6c6d230b48dd34f1aefb
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-10-22 16:18:03 -0400

    Add additional SEVIS tables and supporting packages

---
+++ App/ECA.Database/Sevis/Tables/StudentSecondaryMajorMinor.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [sevis].[StudentSecondaryMajorMinor]
+(
+	[ProgramSubjectId] INT IDENTITY(1,1) NOT NULL, 
+    [ProgramSubjectCode] CHAR(7) NOT NULL, 
+    [Description] NVARCHAR(200) NOT NULL, 
+    [History_CreatedBy] INT NOT NULL, 
+    [History_CreatedOn] DATETIMEOFFSET NOT NULL, 
+    [History_RevisedBy] INT NOT NULL, 
+    [History_RevisedOn] DATETIMEOFFSET NOT NULL, 
+    CONSTRAINT [PK_sevis.ProgramSubject] PRIMARY KEY ([ProgramSubjectId])
+)
+
+GO
+
+CREATE INDEX [IX_ProgramSubjectCode] ON [sevis].[StudentSecondaryMajorMinor] ([ProgramSubjectCode])
