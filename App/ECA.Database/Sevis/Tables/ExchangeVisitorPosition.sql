5c76de428e1bb112892681e3a99f69eabaf8f268
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-10-22 19:40:14 -0400

    New SEVIS tables

---
+++ App/ECA.Database/Sevis/Tables/ExchangeVisitorPosition.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [sevis].[ExchangeVisitorPosition]
+(
+	[ExchangeVisitorPositionId] INT IDENTITY(1,1) NOT NULL, 
+    [PositionCode] NVARCHAR(4) NOT NULL, 
+    [Description] NVARCHAR(200) NOT NULL, 
+    [History_CreatedBy] INT NOT NULL, 
+    [History_CreatedOn] DATETIMEOFFSET NOT NULL, 
+    [History_RevisedBy] INT NOT NULL, 
+    [History_RevisedOn] DATETIMEOFFSET NOT NULL, 
+    CONSTRAINT [PK_sevis.ExchangeVisitorPosition] PRIMARY KEY ([ExchangeVisitorPositionId])
+)
+
+GO
+
+CREATE INDEX [IX_ExchangeVisitorPositionCode] ON [sevis].[ExchangeVisitorPosition] ([PositionCode])
+
+GO
+
+CREATE INDEX [IX_Description] ON [sevis].[Position] ([Description])
