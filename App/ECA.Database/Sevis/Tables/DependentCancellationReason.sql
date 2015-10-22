0baaa617364eec952e53d224ae31215a53b935b0
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-10-22 13:03:28 -0400

    Changes and New SSIS Package; New Sevis Tables

---
+++ App/ECA.Database/Sevis/Tables/DependentCancellationReason.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [sevis].[DependentCancellationReason]
+(
+	[DependentCancellationReasonId] INT IDENTITY(1,1) NOT NULL, 
+    [ReasonCode] NVARCHAR(2) NOT NULL, 
+    [Description] NVARCHAR(100) NOT NULL, 
+    [History_CreatedBy] INT NOT NULL, 
+    [History_CreatedOn] DATETIMEOFFSET NOT NULL, 
+    [History_RevisedBy] INT NOT NULL, 
+    [History_RevisedOn] DATETIMEOFFSET NOT NULL, 
+    CONSTRAINT [PK_sevis.DependentCancellationReason] PRIMARY KEY ([DependentCancellationReasonId])
+)
+
+GO
+
+CREATE INDEX [IX_DependentCancellationReasonCode] ON [sevis].[DependentCancellationReason] ([ReasonCode])
---
+++ App/ECA.Database/Sevis/Tables/EmploymentTime.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [sevis].[EmploymentTime]
+(
+	[EmploymentTimeId] INT IDENTITY(1,1) NOT NULL, 
+    [EmploymentTimeCode] CHAR(2) NOT NULL, 
+    [Description] NVARCHAR(50) NOT NULL, 
+    [History_CreatedBy] INT NOT NULL, 
+    [History_CreatedOn] DATETIMEOFFSET NOT NULL, 
+    [History_RevisedBy] INT NOT NULL, 
+    [History_RevisedOn] DATETIMEOFFSET NOT NULL, 
+    CONSTRAINT [PK_sevis.EmploymentTime] PRIMARY KEY ([EmploymentTimeId])
+)
+
+GO
+
+CREATE INDEX [IX_EmploymentTimeCode] ON [sevis].[EmploymentTime] ([EmploymentTimeCode])
