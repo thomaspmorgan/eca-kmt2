bf0a4341cb1f57f0b9ad2476a388302bd644d236
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-10-21 23:54:01 -0400

    Expand StatusName column

--- App/ECA.Database/dbo/Tables/ProjectStatus.sql
+++ App/ECA.Database/dbo/Tables/ProjectStatus.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[ProjectStatus] (
     [ProjectStatusId]   INT                IDENTITY (1, 1) NOT NULL,
-    [Status]            NVARCHAR (20)      NOT NULL,
+    [Status]            NVARCHAR (50)      NOT NULL,
     [History_CreatedBy] INT                NOT NULL,
     [History_CreatedOn] DATETIMEOFFSET (7) NOT NULL,
     [History_RevisedBy] INT                NOT NULL,
