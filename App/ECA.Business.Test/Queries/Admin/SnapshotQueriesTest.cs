d94486852181d7574799284c8f8f6bffff4c4ee7
Wassim Boustani <wassim.boustani@buchanan-edwards.com>
2015-11-03 15:48:32 -0500

    snapshot test updates.

--- App/ECA.Business.Test/Queries/Admin/SnapshotQueriesTest.cs
+++ App/ECA.Business.Test/Queries/Admin/SnapshotQueriesTest.cs
@@ -383,13 +383,196 @@ namespace ECA.Business.Test.Queries.Admin
         [TestMethod]
         public void TestCreateGetProgramAlumniCountQuery()
         {
-            Assert.AreEqual("todo", "todo");
+            var person = new Person
+            {
+                PersonId = 1
+            };
+
+            var person2 = new Person
+            {
+                PersonId = 2
+            };
+
+            var active = new ProgramStatus
+            {
+                ProgramStatusId = ProgramStatus.Active.Id,
+                Status = ProgramStatus.Active.Value
+            };
+
+            var status = new ProjectStatus
+            {
+                ProjectStatusId = 1,
+                Status = "status"
+            };
+
+            var alumni = new ParticipantStatus
+            {
+                ParticipantStatusId = ParticipantStatus.Alumnus.Id,
+                Status = ParticipantStatus.Alumnus.Value
+            };
+
+            var endDate = DateTime.UtcNow;
+
+            var org = new Organization
+            {
+                OrganizationId = 1,
+                OfficeSymbol = "ABCDE"
+            };
+
+            var program = new Program
+            {
+                ProgramId = 1,
+                Owner = org,
+                OwnerId = org.OrganizationId,
+                ProgramStatus = active,
+                ProgramStatusId = active.ProgramStatusId
+            };
+
+            var project = new Project
+            {
+                ProjectId = 1,
+                ParentProgram = program,
+                Status = status,
+                ProjectStatusId = status.ProjectStatusId,
+                EndDate = endDate
+            };
+
+            var participant = new Participant
+            {
+                ParticipantId = 1,
+                Person = person,
+                PersonId = person.PersonId,
+                Project = project,
+                ProjectId = project.ProjectId,
+                ParticipantStatusId = alumni.ParticipantStatusId,
+                StatusDate = endDate
+            };
+
+            var participant2 = new Participant
+            {
+                ParticipantId = 2,
+                Person = person2,
+                PersonId = person2.PersonId,
+                Project = project,
+                ProjectId = project.ProjectId,
+                ParticipantStatusId = alumni.ParticipantStatusId,
+                StatusDate = endDate
+            };
+
+            project.Participants.Add(participant);
+            project.Participants.Add(participant2);
+            program.Projects.Add(project);
+
+            context.Programs.Add(program);
+            context.Projects.Add(project);
+            context.Participants.Add(participant);
+            context.Participants.Add(participant2);
+
+            List<int> programIds = new List<int>();
+            programIds.Add(program.ProgramId);
+
+            var results = SnapshotQueries.CreateGetProgramAlumniCountQuery(context, programIds);
+            Assert.AreEqual(2, results.DataValue);
         }
 
         [TestMethod]
         public void TestCreateGetProgramProminenceCountQuery()
         {
-            Assert.AreEqual("todo", "todo");
+            var active = new ProgramStatus
+            {
+                ProgramStatusId = ProgramStatus.Active.Id,
+                Status = ProgramStatus.Active.Value
+            };
+
+            var status = new ProjectStatus
+            {
+                ProjectStatusId = 1,
+                Status = "status"
+            };
+
+            var endDate = DateTime.UtcNow;
+
+            var org = new Organization
+            {
+                OrganizationId = 1,
+                OfficeSymbol = "ABCDE"
+            };
+
+            var program = new Program
+            {
+                ProgramId = 1,
+                Owner = org,
+                OwnerId = org.OrganizationId,
+                ProgramStatus = active,
+                ProgramStatusId = active.ProgramStatusId
+            };
+
+            var project = new Project
+            {
+                ProjectId = 1,
+                ParentProgram = program,
+                ProgramId = program.ProgramId,
+                Status = status,
+                ProjectStatusId = status.ProjectStatusId,
+                EndDate = endDate
+            };
+
+            List<Program> programs = new List<Program>();
+            programs.Add(program);
+            List<Project> projects = new List<Project>();
+            projects.Add(project);
+
+            var category1 = new Category
+            {
+                CategoryId = 1,
+                CategoryName = "Cat1",
+                Programs = programs,
+                Projects = projects
+            };
+
+            var category2 = new Category
+            {
+                CategoryId = 2,
+                CategoryName = "Cat1",
+                Programs = programs,
+                Projects = projects
+            };
+
+            var category3 = new Category
+            {
+                CategoryId = 3,
+                CategoryName = "Cat1",
+                Programs = programs,
+                Projects = projects
+            };
+
+            var category4 = new Category
+            {
+                CategoryId = 4,
+                CategoryName = "Cat2",
+                Programs = programs,
+                Projects = projects
+            };
+
+            program.Projects.Add(project);
+            project.Categories.Add(category1);
+            project.Categories.Add(category2);
+            project.Categories.Add(category3);
+            project.Categories.Add(category4);
+
+            context.Programs.Add(program);
+            context.Projects.Add(project);
+            context.Categories.Add(category1);
+            context.Categories.Add(category2);
+            context.Categories.Add(category3);
+            context.Categories.Add(category4);
+
+            List<int> programIds = new List<int>();
+            programIds.Add(program.ProgramId);
+
+            List<int> query = SnapshotQueries.CreateGetProgramProminenceCountQuery(context, programIds).ToList();
+            var catIds = query.Select(x => x).Distinct().Count();
+            Assert.AreEqual(4, catIds);
         }
 
         [TestMethod]
@@ -493,7 +676,82 @@ namespace ECA.Business.Test.Queries.Admin
         [TestMethod]
         public void TestCreateGetProgramTopThemesQuery()
         {
-            Assert.AreEqual("todo", "todo");
+            var active = new ProgramStatus
+            {
+                ProgramStatusId = ProgramStatus.Active.Id,
+                Status = ProgramStatus.Active.Value
+            };
+
+            var status = new ProjectStatus
+            {
+                ProjectStatusId = 1,
+                Status = "status"
+            };
+
+            var endDate = DateTime.UtcNow;
+
+            var org = new Organization
+            {
+                OrganizationId = 1,
+                OfficeSymbol = "ABCDE"
+            };
+
+            var program = new Program
+            {
+                ProgramId = 1,
+                Owner = org,
+                OwnerId = org.OrganizationId,
+                ProgramStatus = active,
+                ProgramStatusId = active.ProgramStatusId
+            };
+
+            List<Program> programs = new List<Program>();
+            programs.Add(program);
+
+            var theme1 = new Theme
+            {
+                ThemeId = 1,
+                ThemeName = "Theme 1",
+                Programs = programs                
+            };
+
+            var theme2 = new Theme
+            {
+                ThemeId = 2,
+                ThemeName = "Theme 1",
+                Programs = programs
+            };
+
+            var theme3 = new Theme
+            {
+                ThemeId = 3,
+                ThemeName = "Theme 1",
+                Programs = programs
+            };
+
+            var theme4 = new Theme
+            {
+                ThemeId = 4,
+                ThemeName = "Theme 2",
+                Programs = programs
+            };
+
+            program.Themes.Add(theme1);
+            program.Themes.Add(theme2);
+            program.Themes.Add(theme3);
+            program.Themes.Add(theme4);
+            context.Programs.Add(program);
+            context.Themes.Add(theme1);
+            context.Themes.Add(theme2);
+            context.Themes.Add(theme3);
+            context.Themes.Add(theme4);
+            
+            List<int> programIds = new List<int>();
+            programIds.Add(program.ProgramId);
+
+            List<string> results = SnapshotQueries.CreateGetProgramTopThemesQuery(context, programIds).Result.ToList();
+            Assert.AreEqual(0, results.FindIndex(t => t == "Theme 1"));
+            Assert.AreEqual(1, results.FindIndex(t => t == "Theme 2"));
         }
 
         [TestMethod]
@@ -505,7 +763,121 @@ namespace ECA.Business.Test.Queries.Admin
         [TestMethod]
         public void TestCreateGetProgramParticipantsByYearQuery()
         {
-            Assert.AreEqual("todo", "todo");
+            var person = new Person
+            {
+                PersonId = 1
+            };
+
+            var person2 = new Person
+            {
+                PersonId = 2
+            };
+
+            var person3 = new Person
+            {
+                PersonId = 3
+            };
+
+            var active = new ProgramStatus
+            {
+                ProgramStatusId = ProgramStatus.Active.Id,
+                Status = ProgramStatus.Active.Value
+            };
+
+            var status = new ProjectStatus
+            {
+                ProjectStatusId = 1,
+                Status = "status"
+            };
+
+            DateTime endDate = DateTime.UtcNow;
+            DateTime oldestDate = DateTime.UtcNow.AddYears(-5);
+            DateTime project1StartDate = oldestDate.AddYears(2);
+            DateTime project2StartDate = oldestDate.AddYears(3);
+
+            var org = new Organization
+            {
+                OrganizationId = 1,
+                OfficeSymbol = "ABCDE"
+            };
+
+            var program = new Program
+            {
+                ProgramId = 1,
+                Owner = org,
+                OwnerId = org.OrganizationId,
+                ProgramStatus = active,
+                ProgramStatusId = active.ProgramStatusId
+            };
+
+            var project = new Project
+            {
+                ProjectId = 1,
+                ParentProgram = program,
+                ProgramId = program.ProgramId,
+                Status = status,
+                ProjectStatusId = status.ProjectStatusId,
+                StartDate = project1StartDate,
+                EndDate = endDate
+            };
+
+            var project2 = new Project
+            {
+                ProjectId = 2,
+                ParentProgram = program,
+                ProgramId = program.ProgramId,
+                Status = status,
+                ProjectStatusId = status.ProjectStatusId,
+                StartDate = project2StartDate,
+                EndDate = endDate
+            };
+
+            var participant = new Participant
+            {
+                ParticipantId = 1,
+                Person = person,
+                PersonId = person.PersonId,
+                Project = project,
+                ProjectId = project.ProjectId
+            };
+
+            var participant2 = new Participant
+            {
+                ParticipantId = 2,
+                Person = person2,
+                PersonId = person2.PersonId,
+                Project = project,
+                ProjectId = project.ProjectId
+            };
+
+            var participant3 = new Participant
+            {
+                ParticipantId = 3,
+                Person = person3,
+                PersonId = person3.PersonId,
+                Project = project2,
+                ProjectId = project2.ProjectId
+            };
+
+            project.Participants.Add(participant);
+            project.Participants.Add(participant2);
+            project2.Participants.Add(participant3);
+            program.Projects.Add(project);
+            program.Projects.Add(project2);
+
+            context.Programs.Add(program);
+            context.Projects.Add(project);
+            context.Projects.Add(project2);
+            context.Participants.Add(participant);
+            context.Participants.Add(participant2);
+            context.Participants.Add(participant3);
+
+            List<int> programIds = new List<int>();
+            programIds.Add(program.ProgramId);
+
+            var results = SnapshotQueries.CreateGetProgramParticipantsByYearQuery(context, programIds);
+            Assert.IsTrue(results.Result.values.Where(x => x.Key == project1StartDate.Year).Select(v => v.Value).FirstOrDefault() == 2);
+            Assert.IsTrue(results.Result.values.Where(x => x.Key == project2StartDate.Year).Select(v => v.Value).FirstOrDefault() == 1);
         }
 
         [TestMethod]
