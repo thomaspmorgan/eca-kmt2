59142b230b4fbe6d2e7af5efc1a7cd517dc4a1f3
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2015-11-04 09:07:40 -0500

    DB Location table changes; Updated IVLP Load SSIS packages

--- App/ECA.Database/dbo/Tables/Location.sql
+++ App/ECA.Database/dbo/Tables/Location.sql
@@ -20,6 +20,7 @@
     [LocationISO-2] NVARCHAR(10) NULL, 
     [City_LocationId] INT NULL, 
     [Division_LocationId] INT NULL, 
+    [IsDivisionUnknown] BIT NULL, 
     CONSTRAINT [PK_dbo.Location] PRIMARY KEY CLUSTERED ([LocationId] ASC),
     CONSTRAINT [FK_dbo.Location_dbo.Location_Country_LocationId] FOREIGN KEY ([Country_LocationId]) REFERENCES [dbo].[Location] ([LocationId]),
     CONSTRAINT [FK_dbo.Location_dbo.Location_Region_LocationId] FOREIGN KEY ([Region_LocationId]) REFERENCES [dbo].[Location] ([LocationId]),
