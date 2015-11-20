91b586a5ed3a2b94ea4f9e13b60009ab618f4a30
Wassim Boustani <Wassim.Boustani@buchanan-edwards.com>
2015-11-20 15:50:23 -0500

    sevis validation updates.

--- App/ECA.Business/Service/Persons/PersonSevisServiceValidator.cs
+++ App/ECA.Business/Service/Persons/PersonSevisServiceValidator.cs
@@ -1,8 +1,8 @@
 ﻿using ECA.Business.Validation;
 using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using System.Xml.Serialization;
+//using System.Data;
+//using System.IO;
+//using System.Xml.Serialization;
 
 namespace ECA.Business.Service.Persons
 {
@@ -25,27 +25,26 @@ namespace ECA.Business.Service.Persons
                 yield return new SevisValidationResult<PersonSevisServiceValidationEntity>(x => x.sevisPerson, PERSON_NOT_FOUND);
             }
 
-            var xsdPath = System.AppDomain.CurrentDomain.BaseDirectory;
-
-            DataSet MyDataSet = new DataSet();
-            MyDataSet.ReadXmlSchema(@"schema.xsd");
-
-            string entityXml = SerializeToXmlString(validationEntity);
-            MyDataSet.ReadXml(entityXml);
 
 
 
+
+
+            //var xsdPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            //DataSet MyDataSet = new DataSet();
+            //MyDataSet.ReadXmlSchema(@"schema.xsd");
+            //string entityXml = SerializeToXmlString(validationEntity);
+            //MyDataSet.ReadXml(entityXml);            
         }
         
-        private string SerializeToXmlString(UpdatedParticipantPersonSevisValidationEntity validationEntity)
-        {
-            string retVal = string.Empty;
-            TextWriter writer = new StringWriter();
-            XmlSerializer serializer = new XmlSerializer(validationEntity.GetType());
-            serializer.Serialize(writer, validationEntity);
-            retVal = writer.ToString();
-            return retVal;
-        }
-
+        //private string SerializeToXmlString(UpdatedParticipantPersonSevisValidationEntity validationEntity)
+        //{
+        //    string retVal = string.Empty;
+        //    TextWriter writer = new StringWriter();
+        //    XmlSerializer serializer = new XmlSerializer(validationEntity.GetType());
+        //    serializer.Serialize(writer, validationEntity);
+        //    retVal = writer.ToString();
+        //    return retVal;
+        //}
     }
 }
