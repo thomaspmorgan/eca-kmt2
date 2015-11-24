b54026f721d2d812abe52c7dbc83f7f5ce5475c7
briangibowski <brian.gibowski@buchanan-edwards.com>
2015-11-24 10:58:28 -0600

    started stubbing out the travelservice before setting the story to blocked for now
    Related Work Items: #10171

---
+++ App/ECA.Business/Service/Projects/TravelService.cs
@@ -0,0 +1,23 @@
+﻿using ECA.Core.Service;
+using ECA.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECA.Business.Service.Projects
+{
+    public class TravelService : EcaService
+    {
+        public TravelService(EcaContext context, List<ISaveAction> saveActions = null) : base(context, saveActions)
+        {
+            Contract.Requires(context != null, "The context must not be null.");
+        }
+
+        #region Get
+
+        #endregion
+    }
+}
