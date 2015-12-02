6e9754ddb4fa7135dd39b5284b3eba7535041b72
Brandon Tucker <brandon.tucker@buchanan-edwards.com>
2015-12-02 10:16:33 -0500

    Added static data for category property join table

--- App/ECA.Database/Scripts/PostDeploy/ECA.PostDeployment.sql
+++ App/ECA.Database/Scripts/PostDeploy/ECA.PostDeployment.sql
@@ -14,6 +14,7 @@ Post-Deployment Script Template
 :r .\StaticData\dbo.AddressType.staticdata.sql
 :r .\StaticData\dbo.ArtifactType.staticdata.sql
 :r .\StaticData\dbo.DataPointCategory.staticdata.sql
+:r .\StaticData\dbo.DataPointCategoryProperty.staticdata.sql
 :r .\StaticData\dbo.DataPointProperty.staticdata.sql
 :r .\StaticData\dbo.EmailAddressType.staticdata.sql
 :r .\StaticData\dbo.Focus.staticdata.sql
