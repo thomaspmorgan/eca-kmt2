7fbdbf350bc47429a248e9b64d02a8009862c67f
Brandon Tucker <brandon.tucker@buchanan-edwards.com>
2015-12-02 11:54:52 -0500

    Made data point config dynamic
    Related Work Items: #9540

--- App/ECA.WebApp.AngularJS/app/data-points/data-points.controller.js
+++ App/ECA.WebApp.AngularJS/app/data-points/data-points.controller.js
@@ -3,10 +3,10 @@
 angular.module('staticApp')
   .controller('DataPointsCtrl', function ($scope, $q, $modalInstance, parameters, ConstantsService, OfficeService, NotificationService) {
 
-      $scope.showOfficeSection = true;
-      $scope.showProgramSection = true;
-      $scope.showProjectSection = true;
-      $scope.showPersonSection = true;
+      $scope.expandOfficeSection = true;
+      $scope.expandProgramSection = true;
+      $scope.expandProjectSection = true;
+      $scope.expandPersonSection = true;
 
       $scope.close = function () {
           $modalInstance.close();
@@ -27,16 +27,7 @@ angular.module('staticApp')
           if (parameters.resourceType.id === ConstantsService.resourceType.office.id) {
               OfficeService.getDataPointConfigurations(parameters.foreignResourceId)
                 .then(function (response) {
-                    
-                    var dataPointConfigurations = { office: {} };
-                    for (var i = 0; i < response.data.length; i++) {
-                        if (response.data[i].category === "office") {
-                            dataPointConfigurations.office[response.data[i].property] = response.data[i].isHidden;
-                        }
-                    }
-
-                    console.log(dataPointConfigurations);
-
+                    $scope.dataConfigurations = response.data;
                 }, function () {
                     NotificationService.showErrorMessage('Unable to load data point configurations for office with id = ' + parameters.foreignResourceId + ".");
                 });
