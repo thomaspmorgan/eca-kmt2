8fb10f67518f0cd784efe007c8dd2b1d4322189a
briangibowski <brian.gibowski@buchanan-edwards.com>
2015-12-03 11:56:56 -0600

    added version number of client
    Related Work Items: #10551

---
+++ App/ECA.WebApp.AngularJS/app/about/about.controller.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name staticApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the staticApp
+ */
+angular.module('staticApp')
+  .controller('AboutCtrl', function ($rootScope, $scope, $state, $log, AboutService) {
+      $scope.view = {};
+      $scope.view.versionNumber = '';
+      $scope.view.isDebugBuild = false;
+      function isDebugBuild(about) {
+          return about.buildConfiguration === 'Debug';
+      }
+
+      AboutService.get()
+      .then(function (response) {
+          var buildConfiguration = response.data.buildConfiguration;
+          var version = response.data.version;
+          $scope.view.versionNumber = version;
+          $scope.view.isDebugBuild = isDebugBuild(response.data);
+      })
+      .catch(function (response) {
+          $log.error('Unable to load about information.');
+      })
+  });
