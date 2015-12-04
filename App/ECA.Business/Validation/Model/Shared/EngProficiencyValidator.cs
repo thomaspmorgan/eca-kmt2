74258db4b30767d5312c421df37dd041d15335df
Wassim Boustani <Wassim.Boustani@buchanan-edwards.com>
2015-12-04 14:45:16 -0500

    sevis validation updates.

---
+++ App/ECA.Business/Validation/Model/Shared/EngProficiencyValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace ECA.Business.Validation.Model
+{
+    public class EngProficiencyValidator : AbstractValidator<EngProficiency>
+    {
+        public const int REASON_MAX_LENGTH = 500;
+
+        public EngProficiencyValidator()
+        {
+            RuleFor(student => student.EngRequired).NotNull().WithMessage("English Proficiency: English Required indicator is required.");
+            RuleFor(student => student.NotRequiredReason).Length(0, REASON_MAX_LENGTH).WithMessage("English Proficiency: Not Required Reason can be up to " + REASON_MAX_LENGTH.ToString() + " characters");
+        }
+    }
+}
