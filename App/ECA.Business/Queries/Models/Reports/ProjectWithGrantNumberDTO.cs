ba0304fbb30b2f082fd4d358b1761191cc8da136
briangibowski <brian.gibowski@buchanan-edwards.com>
2015-12-17 12:44:54 -0600

    merge

---
+++ App/ECA.Business/Queries/Models/Reports/ProjectWithGrantNumberDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECA.Business.Queries.Models.Reports
+{
+    public class ProjectWithGrantNumberDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Focus { get; set; }
+        public string Location { get; set; }
+        public decimal Amount { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset? EndDate { get; set; }
+        public string GrantNumber { get; set; }
+        public string Organization {get; set;}
+
+    }
+}
