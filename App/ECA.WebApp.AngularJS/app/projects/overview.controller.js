d4289372b5450aefb3c2ac3403264312d1bb321c
briangibowski <brian.gibowski@buchanan-edwards.com>
2015-12-17 15:31:28 -0600

    added lat and long to project overview along with google map and markers
    Related Work Items: #10670

--- App/ECA.WebApp.AngularJS/app/projects/overview.controller.js
+++ App/ECA.WebApp.AngularJS/app/projects/overview.controller.js
@@ -13,6 +13,7 @@ angular.module('staticApp')
         $stateParams,
         $q,
         $log,
+        $compile,
         orderByFilter,
         ProjectService,
         ConstantsService,
@@ -22,11 +23,12 @@ angular.module('staticApp')
       $scope.view.params = $stateParams;
       $scope.view.isLoading = false;
       $scope.view.isLoadingOfficeSettings = false;
+      $scope.view.isMapIdle = false;
       $scope.categoryLabel = "...";
       $scope.objectiveLabel = "...";
       $scope.sortedCategories = [];
       $scope.sortedObjectives = [];
-      
+
       $scope.view.isLoadingOfficeSetting = true;
       $scope.$parent.data.loadOfficeSettingsPromise.promise
         .then(function (settings) {
@@ -47,10 +49,89 @@ angular.module('staticApp')
             $scope.view.isLoadingOfficeSetting = false;
         });
 
+
       $scope.view.isLoading = true;
       $scope.$parent.data.loadProjectByIdPromise.promise.then(function (project) {
+          if ($scope.$parent.project.locations) {
+              $scope.$parent.project.locations = orderByFilter($scope.$parent.project.locations, '+name');
+          }
           $scope.sortedCategories = orderByFilter($scope.$parent.project.categories, '+focusName');
           $scope.sortedObjectives = orderByFilter($scope.$parent.project.objectives, '+justificationName');
           $scope.view.isLoading = false;
+
       });
+
+      var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      var labelIndex = 0;
+      var markers = [];
+      function setMarkers(locations) {
+          var map = getLocationMap();
+          var bounds = new google.maps.LatLngBounds();
+          angular.forEach(locations, function (location, index) {
+              if (location.latitude && location.longitude) {
+                  var latLng = new google.maps.LatLng(location.latitude, location.longitude);
+                  var marker = new google.maps.Marker({
+                      position: latLng,
+                      map: map,
+                      animation: google.maps.Animation.DROP,
+                      title: location.name,
+                      label: labels[labelIndex++ % labels.length],
+                  });
+                  location.mapMarkerLabel = marker.label;
+                  location.marker = marker;
+                  marker.addListener('click', function () {
+                      onMapMarkerClick(marker, location);
+                  });
+                  bounds.extend(latLng);
+                  markers.push(marker);
+              }
+          });
+          map.fitBounds(bounds);
+      }
+
+      function clearMapMarkers() {
+          angular.forEach(markers, function (marker, index) {
+              marker.setMap(null);
+          });
+          markers = [];
+      }
+
+
+      $scope.view.mapOptions = {
+          center: new google.maps.LatLng(38.9071, -77.0368),
+          zoom: 0,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      $scope.view.onMapIdle = function () {
+          $scope.view.isMapIdle = true;
+          if (markers.length === 0) {
+              setMarkers($scope.$parent.project.locations);
+          }
+      }
+
+      $scope.view.onMapMarkerLabelClick = function (location) {
+          var marker = location.marker;
+          var map = getLocationMap();
+          map.setCenter(marker.getPosition());
+      }
+
+      var infoWindow = null;
+      function onMapMarkerClick(marker, location) {
+          var map = getLocationMap();
+          $scope.mapLocation = location;
+          
+          var content = '<div><ng-include src="\'app/projects/project-location-marker.html\'"></ng-include></div>';
+          var compiled = $compile(content)($scope);
+          if (infoWindow) {
+              infoWindow.close();
+          }
+          infoWindow = new google.maps.InfoWindow();
+          infoWindow.setContent(compiled[0]);
+          infoWindow.open(map, marker);
+      }
+
+      function getLocationMap() {
+          return $scope.view.locationMap;
+      }
   });
