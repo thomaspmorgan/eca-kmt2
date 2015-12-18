a2f6459c8f9dcb6259a5207cd52f20b5f277413f
briangibowski <brian.gibowski@buchanan-edwards.com>
2015-12-18 10:04:13 -0600

    added tables for itinerary group and itinerary group participants
    Related Work Items: #9747

---
+++ App/ECA.Database/dbo/Tables/ItineraryGroupParticipant.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ItineraryGroupParticipant]
+(
+	[ItineraryGroupId] INT NOT NULL,
+	[ParticipantId] INT NOT NULL,
+
+	CONSTRAINT [PK_dbo.ItineraryGroupParticipant] PRIMARY KEY CLUSTERED ([ItineraryGroupId] ASC, [ParticipantId] ASC),
+	CONSTRAINT [FK_dbo.ItineraryGroupParticipant_dbo.ItineraryGroup_ItineraryGroupId] FOREIGN KEY ([ItineraryGroupId]) REFERENCES [dbo].[ItineraryGroup]([ItineraryGroupId]),
+	CONSTRAINT [FK_dbo.ItineraryGroupParticipant_dbo.Participant_ParticipantId] FOREIGN KEY ([ParticipantId]) REFERENCES [dbo].[Participant]([ParticipantId])
+)
