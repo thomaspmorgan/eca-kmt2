9ac07dc28ab0c33f905fc18177cb7706ef0daa6a
Wassim Boustani <Wassim.Boustani@buchanan-edwards.com>
2015-12-22 10:18:43 -0500

    sevis validation

---
+++ App/ECA.Business/Validation/Model/Shared/AddTIPPUpdate.cs
@@ -0,0 +1,21 @@
+﻿using ECA.Business.Validation.Model.CreateEV;
+
+namespace ECA.Business.Validation.Model.Shared
+{
+    public class AddTIPPUpdate
+    {
+        public AddTIPPUpdate()
+        {
+            ParticipantInfo = new ParticipantInfoUpdate();
+            TippSite = new TippSite();
+        }
+
+        public bool print7002 { get; set; }
+
+        public ParticipantInfoUpdate ParticipantInfo { get; set; }
+
+        public TippSite TippSite { get; set; }
+
+
+    }
+}
---
+++ App/ECA.Business/Validation/Model/Shared/SiteOfActivityUpdate.cs
@@ -0,0 +1,20 @@
+﻿using ECA.Business.Validation.Model.CreateEV;
+
+namespace ECA.Business.Validation.Model.Shared
+{
+    public class SiteOfActivityUpdate
+    {
+        public SiteOfActivitySOA AddSOA { get; set; }
+
+        public SiteOfActivityExemptUpdate AddEXEMPT { get; set; }
+
+        public SiteOfActivitySeekingGemp AddSEEKINGEMP { get; set; }
+
+        public SiteOfActivityAddOnTravel AddONTRAVEL { get; set; }
+
+        public DeleteSiteOfActivity Delete { get; set; }
+
+        public EditSiteOfActivity Edit { get; set; }
+        
+    }
+}
