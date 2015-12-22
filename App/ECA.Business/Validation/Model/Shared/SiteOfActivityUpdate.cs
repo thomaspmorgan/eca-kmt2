bb15bc50ff7076ff453daf09c0737490dccdc815
Wassim Boustani <Wassim.Boustani@buchanan-edwards.com>
2015-12-22 15:31:28 -0500

    sevis updates

--- App/ECA.Business/Validation/Model/Shared/SiteOfActivityUpdate.cs
+++ App/ECA.Business/Validation/Model/Shared/SiteOfActivityUpdate.cs
@@ -1,19 +1,45 @@
 ﻿using ECA.Business.Validation.Model.CreateEV;
+using System.Xml.Serialization;
 
 namespace ECA.Business.Validation.Model.Shared
 {
+    /// <summary>
+    /// Site of activity events
+    /// </summary>
     public class SiteOfActivityUpdate
     {
+        /// <summary>
+        /// Add site of activity
+        /// </summary>
+        [XmlArrayItem("Add xsi:type=\"AddSOA\"")]
         public SiteOfActivitySOA AddSOA { get; set; }
 
+        /// <summary>
+        /// Site of activity indicating site is exempt
+        /// </summary>
+        [XmlArrayItem("Add xsi:type=\"AddEXEMPT\"")]
         public SiteOfActivityExemptUpdate AddEXEMPT { get; set; }
 
+        /// <summary>
+        /// Site of activity indicating site is seeking employment
+        /// </summary>
+        [XmlArrayItem("Add xsi:type=\"AddSEEKINGEMP\"")]
         public SiteOfActivitySeekingGemp AddSEEKINGEMP { get; set; }
 
+        /// <summary>
+        /// Site of activity indicating site is on travel
+        /// </summary>
+        [XmlArrayItem("Add xsi:type=\"AddONTRAVEL\"")]
         public SiteOfActivityAddOnTravel AddONTRAVEL { get; set; }
 
+        /// <summary>
+        /// Delete site of activity
+        /// </summary>
         public DeleteSiteOfActivity Delete { get; set; }
 
+        /// <summary>
+        /// Edit site of activity
+        /// </summary>
         public EditSiteOfActivity Edit { get; set; }        
     }
 }
