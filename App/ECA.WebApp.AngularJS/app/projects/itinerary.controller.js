f1c8a6ce9f197e678f3df863c5f8ca0cf17449b7
Brian Gibowski <brian.gibowski@buchanan-edwards.com>
2016-01-15 09:30:15 -0600

    code cleanup
    Related Work Items: #10821

--- App/ECA.WebApp.AngularJS/app/projects/itinerary.controller.js
+++ App/ECA.WebApp.AngularJS/app/projects/itinerary.controller.js
@@ -377,7 +377,6 @@ angular.module('staticApp')
       }
 
       function addEvent(itineraryStop) {
-          //$scope.view.eventSources[0].push(getEvent(itineraryStop));
           $scope.view.eventSources[0].push(itineraryStop.getEvent());
       }
 
@@ -386,19 +385,6 @@ angular.module('staticApp')
               $scope.view.eventSources[0].splice(i, 1);
           }
       }
-
-      //function getEvent(itineraryStop) {
-      //    var start = moment(itineraryStop.arrivalDate);
-      //    var end = moment(itineraryStop.departureDate);
-      //    return {
-      //        title: itineraryStop.name,
-      //        start: start,
-      //        end: end,
-      //        allDay: false,
-      //        itineraryStopId: itineraryStop.itineraryStopId,
-      //        color: itineraryStop.color
-      //    };
-      //}
 
       function onCalendarItemClick(calEvent, jsEvent, view) {
           var itineraryStop = null;
@@ -452,11 +438,6 @@ angular.module('staticApp')
           });
           return itineraryStops;
       }
-
-      //var colors = ["#d35304", "#8e9900", "#689e0c", "#640096", "#dd067d", "#028287", "#ea007d", "#024c60", "#0da514", "#e216af", "#9e0910", "#0b6293", "#51a508", "#1c9e0e", "#dbb702", "#078435", "#8c0c21", "#460f9e", "#319e03", "#064260", "#bc0b6f", "#0da591", "#0f0666", "#2c930d", "#054175", "#c405a1", "#9e0803", "#02426b", "#6e9601", "#576d00", "#099620", "#0c7287", "#89a309", "#e05d06", "#98a004", "#027f5c", "#0e8c5d", "#2a7a00", "#071f8c", "#e50d80", "#017756", "#cc6a14", "#007a78", "#056482", "#007267", "#af0e08", "#076677", "#9b1d07", "#350d84", "#87011e", "#0b5570", "#e56814", "#8c0c35", "#69960f", "#646d02", "#4a820a", "#af001d", "#ad1608", "#068934", "#023e70", "#efbc02", "#ce0ca1", "#e008ae", "#ef09ef", ];
-      //function setItineraryStopColor(itineraryStop) {
-      //    itineraryStop.color = colors[colorIndex++ % colors.length];
-      //}
 
       function initializeItinerary(itinerary) {
           toDate(itinerary, 'startDate');
