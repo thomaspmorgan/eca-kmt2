dfcca7adcfd62057815e2b73299444892891f33b
Tom Morgan <thomas.morgan@buchanan-edwards.com>
2016-01-18 12:59:55 -0500

    SEVIS batch processing table

---
+++ App/ECA.Database/dbo/Tables/SevisBatchProcessing.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SevisBatchProcessing]
+(
+	[BatchId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SubmitDate] DATETIMEOFFSET NOT NULL, 
+    [RetrieveDate] DATETIMEOFFSET NULL, 
+    [SendXml] XML NOT NULL, 
+    [TransactionLogXml] XML NULL, 
+    [UploadDispositionCode] NCHAR(5) NULL, 
+    [ProcessDispositionCode] NCHAR(5) NULL, 
+    [DownloadDispositionCode] NCHAR(5) NULL
+)
