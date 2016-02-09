19c8655e39a42f3abdd98464711a308299147317
Marc Setien <Marc.Setien@Buchanan-Edwards.com>
2016-02-09 18:19:37 -0500

    Created PersonType table and added PersonTypeId, CE_PersonId columns to Person table

---
+++ App/ECA.Database/dbo/Tables/PersonType.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[PersonType] (
+    [PersonTypeId] INT            IDENTITY (1, 1) NOT NULL,
+    [Name]              NVARCHAR (150) NOT NULL,
+    CONSTRAINT [PK_dbo.PersonType] PRIMARY KEY CLUSTERED ([PersonTypeId] ASC)
+);
