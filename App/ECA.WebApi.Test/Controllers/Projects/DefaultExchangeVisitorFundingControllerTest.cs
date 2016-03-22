480750e4e4ad671c14d310aefba0bb4774233a8d
Brandon Tucker <brandon.tucker@buchanan-edwards.com>
2016-03-22 16:56:47 -0400

    Added webapi unit tests

--- App/ECA.WebApi.Test/Controllers/Projects/DefaultExchangeVisitorFundingControllerTest.cs
+++ App/ECA.WebApi.Test/Controllers/Projects/DefaultExchangeVisitorFundingControllerTest.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using ECA.Business.Queries.Models.Admin;
 using System.Web.Http.Results;
 using ECA.WebApi.Security;
+using ECA.WebApi.Models.Projects;
 
 namespace ECA.WebApi.Test.Controllers.Projects
 {
@@ -21,6 +22,7 @@ namespace ECA.WebApi.Test.Controllers.Projects
         public void TestInit()
         {
             service = new Mock<IDefaultExchangeVisitorFundingService>();
+            userProvider = new Mock<IUserProvider>();
             controller = new DefaultExchangeVisitorFundingController(service.Object, userProvider.Object);
         }
 
@@ -42,6 +44,25 @@ namespace ECA.WebApi.Test.Controllers.Projects
             var response = await controller.GetDefaultExchangeVisitorFundingByIdAsync(1);
             Assert.IsInstanceOfType(response, typeof(NotFoundResult));
             service.Verify(x => x.GetDefaultExchangeVisitorFundingAsync(It.IsAny<int>()));
+        }
+
+        [TestMethod]
+        public async Task TestPutDefaultExchangeVisitorFunding_InvalidModel()
+        {
+            controller.ModelState.AddModelError("key", "error");
+            var response = await controller.PutDefaultExchangeVisitorFunding(1, new UpdatedDefaultExchangeVisitorFundingBindingModel());
+            Assert.IsInstanceOfType(response, typeof(InvalidModelStateResult));
+        }
+
+        [TestMethod]
+        public async Task TestPutDefaultExchangeVisitorFunding()
+        {
+            userProvider.Setup(x => x.GetBusinessUser(It.IsAny<IWebApiUser>())).Returns(new Business.Service.User(0));
+            service.Setup(x => x.UpdateAsync(It.IsAny<UpdatedDefaultExchangeVisitorFunding>())).Returns(Task.FromResult<object>(null));
+            service.Setup(x => x.SaveChangesAsync()).ReturnsAsync(1);
+            var response = await controller.PutDefaultExchangeVisitorFunding(1, new UpdatedDefaultExchangeVisitorFundingBindingModel());
+            Assert.IsInstanceOfType(response, typeof(OkNegotiatedContentResult<DefaultExchangeVisitorFundingDTO>));
+            service.Verify(x => x.SaveChangesAsync(), Times.Once());
         }
     }
 }
