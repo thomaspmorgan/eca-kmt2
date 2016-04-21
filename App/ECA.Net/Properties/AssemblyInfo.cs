13762067f9dc2902bd2b45e0b2b78de4be70b6c9
Tom Morgan <thomas.morgan@buchanan-edwards.com>
2016-04-21 09:21:03 -0400

    Refactored SEVIS communication to ECA.Net namespace, started unit tests

--- App/ECA.Net/Properties/AssemblyInfo.cs
+++ App/ECA.Net/Properties/AssemblyInfo.cs
@@ -34,3 +34,8 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("ECA.Business.SevisBatch.Tests")]
+[assembly: InternalsVisibleTo("ECA.Business.Sevis.Explorables")]
+[assembly: InternalsVisibleTo("ECA.Net.Tests")]
+[assembly: InternalsVisibleTo("ECA.Net.Explorables")]
+
