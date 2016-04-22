122b2903549e831609277e729f12b219be4f9e53
Tom Morgan <thomas.morgan@buchanan-edwards.com>
2016-04-22 18:59:11 -0400

    Renamed files

---
+++ App/ECA.Net/IEcaHttpMessageHandlerService.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace ECA.Net
+{
+    public interface IEcaHttpMessageHandlerService
+    {
+       HttpMessageHandler GetHttpMessageHandler(string thumbprint);
+    }
+}
