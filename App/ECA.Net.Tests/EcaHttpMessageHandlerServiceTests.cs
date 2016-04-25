a42f8c48c2b1991a8711e8cf3debe52cfeda3cc6
Tom Morgan <thomas.morgan@buchanan-edwards.com>
2016-04-25 08:49:12 -0400

    Removed Pex references

--- App/ECA.Net.Tests/EcaHttpMessageHandlerServiceTests.cs
+++ App/ECA.Net.Tests/EcaHttpMessageHandlerServiceTests.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 
-namespace ECA.Net.Tests
+namespace ECA.Net.Test
 {
     [TestClass()]
     public class EcaHttpMessageHandlerServiceTests
