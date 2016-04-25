f8439ff4a3a132d166af8dc3cd8f2f14f4867b5c
Sam Boustani <sam.boustani@gmail.com>
2016-04-25 14:41:13 -0400

    personal info sevis edit enable/disable by status.

--- App/ECA.WebApp.AngularJS/app/people/personal-information.controller.js
+++ App/ECA.WebApp.AngularJS/app/people/personal-information.controller.js
@@ -7,127 +7,34 @@ angular.module('staticApp')
   .controller('PersonInformationCtrl', function ($scope, $stateParams, $state, $timeout, $q, $filter, $log,
                                                 smoothScroll, MessageBox, StateService, ConstantsService, ParticipantPersonsService, NotificationService) {
 
-      $scope.showEvalNotes = true;
-      $scope.showEduEmp = true;
-      $scope.showGeneral = true;
-      $scope.showPii = false;
-      $scope.showContact = true;
       $scope.edit = {};
       $scope.edit.General = false;
       $scope.edit.Pii = false;
       $scope.edit.Contact = false;
       $scope.edit.EduEmp = false;
+      $scope.showEvalNotes = true;
+      $scope.showEduEmp = true;
+      $scope.showGeneral = true;
+      $scope.showContact = true;
       $scope.sevisStatus = { statusName: "", statusNameId: 0 };
 
       var notifyStatuses = ConstantsService.sevisStatusIds.split(',');
-
-      $scope.editGeneral = function () {
-          return CreateMessageBox($scope.edit.General)
-          .then(function (response) {
-              $scope.edit.General = response;
-          });
-      }
-
-      $scope.editPii = function () {
-          return CreateMessageBox($scope.edit.Pii)
-          .then(function (response) {
-              $scope.edit.Pii = response;
-          });
-      }
-
-      $scope.editContact = function () {
-          return CreateMessageBox($scope.edit.Contact)
-          .then(function (response) {
-              $scope.edit.Contact = response;
-          });
-      }
-
-      $scope.editEduEmp = function () {
-          return CreateMessageBox($scope.edit.EduEmp)
-          .then(function (response) {
-              $scope.edit.EduEmp = response;
-          });
-      }
       
       ParticipantPersonsService.getParticipantPersonById($stateParams.personId)
           .then(function (data) {
               $scope.sevisStatus.statusName = data.data.sevisStatus;
-              $scope.sevisStatus.statusNameId = data.data.sevisStatusId;
+              $scope.sevisStatus.statusNameId = data.data.sevisStatusId;              
           }, function (error) {
               $log.error('Unable to load participant info for ' + $stateParams.personId + '.');
               NotificationService.showErrorMessage('Unable to load participant info for ' + $stateParams.personId + '.');
           });
-
-      function CreateMessageBox(userSection) {
-          var defer = $q.defer();
-          if (notifyStatuses.indexOf($scope.sevisStatus.statusNameId.toString()) !== -1) {
-              MessageBox.confirm({
-                  title: 'Confirm Edit',
-                  message: 'The SEVIS participant status of this person is ' + $scope.sevisStatus.statusName + '. Are you sure you want to edit?',
-                  okText: 'Yes',
-                  cancelText: 'No',
-                  okCallback: function () {
-                      userSection = true;
-                      defer.resolve(userSection);
-                  },
-                  cancelCallback: function () {
-                      userSection = false;
-                      defer.resolve(userSection);
-                  }
-              });
-          } else {
-              userSection = !userSection;
-              defer.resolve(userSection);
-          }
-
-          return defer.promise;
-      }
       
-      function handleSectionState() {
-
-          if ($state.current.name === StateService.stateNames.people.section.pii) {
-              scrollToSection('pii',
-                  collapseAllSections,
-                  function (element) {
-                      $scope.showPii = true;
-                  });
-          }
-          else if ($state.current.name === StateService.stateNames.people.section.general) {
-              scrollToSection('general',
-                  collapseAllSections,
-                  function (element) {
-                      $scope.showGeneral = true;
-                  });
-          }
-          else if ($state.current.name === StateService.stateNames.people.section.contact) {
-              scrollToSection('contact',
-                  collapseAllSections,
-                  function (element) {
-                      $scope.showContact = true;
-                  });
+      $scope.isDisabled = function () {
+          if (notifyStatuses.indexOf($scope.sevisStatus.statusNameId.toString()) !== -1) {
+              return true;
+          } else {
+              return false;
           }
       }
 
-      function collapseAllSections() {
-          $scope.showGeneral = false;
-          $scope.showPii = false;
-          $scope.showContact = false;
-          $scope.showEduEmp = false;
-          $scope.showEvalNotes = false;
-        }
-
-      function scrollToSection(sectionId, callbackBefore, callbackAfter) {
-              $timeout(function () {
-              var section = document.getElementById(sectionId);
-                  var options = {
-                      duration: 500,
-                      easing: 'easeIn',
-                  offset: 175,
-                  callbackBefore: callbackBefore,
-                  callbackAfter: callbackAfter
-                  }
-                  smoothScroll(section, options);
-              });
-          }
-      handleSectionState();
   });
