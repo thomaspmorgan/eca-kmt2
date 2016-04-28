458a6f890e61df86c5f62623cbc3c20ebed470b2
briangibowski <brian.gibowski@buchanan-edwards.com>
2016-04-28 08:10:51 -0500

    additional sevis accounts script refactored and sarah shields account has been added
    Related Work Items: #10806

--- App/CAM.Database/Scripts/PostDeploy/AdditionalSevisAccounts.sql
+++ App/CAM.Database/Scripts/PostDeploy/AdditionalSevisAccounts.sql
@@ -1,30 +1,63 @@
+DECLARE @sevisUserAccount TABLE(	
+	[OrgId] nvarchar(15),
+	[Username] nvarchar(10),
+	[EmailAddress] NVARCHAR(200) NOT NULL
+)
+
+--CREATE BE Sevis Account Info
 DECLARE @sevisUsername VARCHAR(10) = 'esayya9302'
 DECLARE @orgId VARCHAR(15) = 'P-1-19833'
 
-DECLARE @thomasMorganAdGuid UNIQUEIDENTIFIER  = convert(uniqueidentifier, '178C83B3-1061-436E-89FE-59073312950F');
-DECLARE @brianGibowskiAdGuid UNIQUEIDENTIFIER  = convert(uniqueidentifier, '86101ACC-10B0-4E10-AEEA-FFCC7B42BBC5');
-DECLARE @brandonTuckerAdGuid UNIQUEIDENTIFIER  = convert(uniqueidentifier, 'E8FEAB56-B7EC-409A-BAF2-0F9F780027DD');
+--Add All BE Accounts Here
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'gibowskibr@statedept.us');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'morgant@statedept.us');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'tuckerb@statedept.us');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'comptonwa@statedept.us');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'sayyade@statedept.us');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'boustaniw@statedept.us');
 
-DECLARE @thomasMorganUserId INT = 0;
-DECLARE @brianGibowskiUserId INT = 0;
-DECLARE @brandonTuckerUserId INT = 0;
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'brian.gibowski@buchanan-edwards.com');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'brandon.tucker@buchanan-edwards.com');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'thomas.morgan@buchanan-edwards.com');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'thomas.morgan@buchanan-edwards.com');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'alan.compton@buchanan-edwards.com');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'wassim.boustani@buchanan-edwards.com');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'jefferson.baker@buchanan-edwards.com');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'eiad.sayyad@buchanan-edwards.com');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'mak.mendelson@buchanan-edwards.com');
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, @sevisUsername, 'mycal.rolland@buchanan-edwards.com');
 
-SELECT @thomasMorganUserId = PrincipalId FROM cam.UserAccount WHERE AdGuid = @thomasMorganAdGuid;
-SELECT @brianGibowskiUserId = PrincipalId FROM cam.UserAccount WHERE AdGuid = @brianGibowskiAdGuid;
-SELECT @brandonTuckerUserId = PrincipalId FROM cam.UserAccount WHERE AdGuid = @brandonTuckerAdGuid;
+--Add All ECA Accounts Here
+INSERT INTO @sevisUserAccount (OrgId, Username, EmailAddress) VALUES (@orgId, 'sshiel1521', 'ShieldsSD@statedept.us');
 
-DECLARE @tblTempTable TABLE (
-	[PrincipalId] int,
+
+--END ADDING ALL ACCOUNTS THAT WILL BE UPSERTED...
+
+
+--Declare a temp table to hold sevis accounts to insert.
+DECLARE @principalSevisAccount TABLE (
+	[PrincipalId] int NULL,
 	[OrgId] nvarchar(15),
 	[Username] nvarchar(10)
 )
 
-INSERT INTO @tblTempTable(PrincipalId, OrgId, Username) VALUES (@thomasMorganUserId, @orgId, @sevisUsername);
-INSERT INTO @tblTempTable(PrincipalId, OrgId, Username) VALUES (@brianGibowskiUserId, @orgId, @sevisUsername);
-INSERT INTO @tblTempTable(PrincipalId, OrgId, Username) VALUES (@brandonTuckerUserId, @orgId, @sevisUsername);
+--Insert into to temp table the sevis accounts that will be upserted.
+INSERT INTO @principalSevisAccount(PrincipalId, OrgId, Username)
+SELECT DISTINCT
+	(SELECT PrincipalId FROM cam.UserAccount WHERE EmailAddress = a.EmailAddress) As PrincipalId,
+	a.OrgId as OrgId,
+	a.Username as Username
+FROM @sevisUserAccount a;
+
+--Delete sevisUserAccounts that are not found/null.  This may happen when the user has not yet registered in this environment.
+DELETE @principalSevisAccount WHERE PrincipalId = 0 OR PrincipalId IS NULL;
+
+DELETE FROM cam.SevisAccount WHERE NOT EXISTS (SELECT * FROM @principalSevisAccount psa WHERE cam.SevisAccount.PrincipalId = psa.PrincipalId)
+
+--Upsert sevis user accounts
 BEGIN
 	MERGE cam.SevisAccount AS target
-	USING @tblTempTable AS source
+	USING @principalSevisAccount AS source
 	ON target.PrincipalId = source.PrincipalId
 	WHEN MATCHED THEN
 		UPDATE SET 
